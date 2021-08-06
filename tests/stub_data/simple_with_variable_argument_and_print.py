aac89e993f383a748abec5be73b1b7182ab5587a
Dhruv Mangtani <dhruv.mangtani@gmail.com>
2021-08-06 11:53:24 -0700

    Updated Print Tests

--- tests/stub_data/simple_with_variable_argument_and_print.py
+++ tests/stub_data/simple_with_variable_argument_and_print.py
@@ -25,11 +25,26 @@ line_2 = CallNode(
     code="min(%s, %s)" % (arg_a.code, arg_10.code),
     function_name="min",
     assigned_variable_name="b",
-    arguments=[],
+    arguments=[arg_a, arg_10],
 )
 
 e2 = DirectedEdge(source_node_id=line_1_id, sink_node_id=line_2_id)
 
+arg_b_id = get_new_id()
+
+arg_b = ArgumentNode(id=arg_b_id, session_id=session.uuid, code="b", positional_order=1, value_call_id=line_2_id)
+
+line_3_id = get_new_id()
+line_3 = CallNode(
+    id=line_3_id, 
+    session_id=session.uuid, 
+    code="print(%s)" % (arg_b.code),
+    function_name="print",
+    arguments=[arg_b],
+)
+
+e3 = DirectedEdge(source_node_id=line_2_id, sink_node_id=line_3_id)
+
 simple_with_variable_argument_and_print = Graph(
-    [line_1, arg_literal, arg_a, arg_10, line_2], [e2]
+    [line_1, arg_literal, arg_a, arg_10, line_2, arg_b, line_3], [e2, e3]
 )
