e74c7cf784b934f4198722a144769faca9007424
Dhruv Mangtani <dhruv.mangtani@gmail.com>
2021-08-10 16:37:00 -0700

    Update to pytests

--- lineapy/__init__.py
+++ lineapy/__init__.py
@@ -1,6 +1,5 @@
 from lineapy.data.graph import Graph
-from lineapy.execution.executor import Executor
 
-__all__ = ["Graph", "Executor"]
+__all__ = ["Graph"]
 
 __version__ = "0.0.1"
--- tests/executor_test.py
+++ tests/executor_test.py
@@ -12,26 +12,26 @@ from tests.stub_data.nested_call_graph import nested_call_graph
 
 class TestBasicExecutor(unittest.TestCase):
     # we should probably do a shared setup in the future
-    def simple_graph(self):
+    def test_simple_graph(self):
         # initialize the executor
         e = Executor()
         e.walk(simple_graph)
         a = e.get_value_by_varable_name("a")
         assert a == 11
 
-    def nested_call_graph(self):
+    def test_nested_call_graph(self):
         e = Executor()
         e.walk(nested_call_graph)
         a = e.get_value_by_varable_name("a")
         assert a == 10
 
-    def graph_with_print(self):
+    def test_graph_with_print(self):
         e = Executor()
         e.walk(simple_with_variable_argument_and_print)
         stdout = e.get_stdout()
         assert stdout == "10\n"
 
-    def basic_import(self):
+    def test_basic_import(self):
         """
         some imports are built in, such as "math" or "datetime"
         """
@@ -40,7 +40,7 @@ class TestBasicExecutor(unittest.TestCase):
         b = e.get_value_by_varable_name("b")
         assert b == 5
 
-    def pip_install_import(self):
+    def test_pip_install_import(self):
         """
         other libs, like pandas, or sckitlearn, need to be pip installed.
         """
@@ -49,16 +49,16 @@ class TestBasicExecutor(unittest.TestCase):
         df = e.get_value_by_varable_name("df")
         assert df is not None
 
-    def program_with_mutations(self):
+    def test_program_with_mutations(self):
         """
         WAIT: need types to be more stable for representing mutation
         """
         pass
 
-    def program_with_loops(self):
+    def test_program_with_loops(self):
         pass
 
-    def program_with_conditionals(self):
+    def test_program_with_conditionals(self):
         pass
 
 if __name__ == "__main__":
