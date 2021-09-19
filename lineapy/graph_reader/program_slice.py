d4dc93c8c30cfded863d5475a05a31593f7d2832
dorx <doris.s.xin@gmail.com>
2021-09-19 15:38:10 -0700

    missed one

--- lineapy/graph_reader/program_slice.py
+++ lineapy/graph_reader/program_slice.py
@@ -1,11 +1,103 @@
+import re
+
 from lineapy import Graph
+from lineapy.data.types import Node
 from lineapy.graph_reader.base import GraphReader
+from lineapy.graph_reader.graph_util import get_segment_from_code
 
 
-class ProgramSliceGraphReader(GraphReader):
-    """
-    TODO
-    """
+class ProgramSlicer(GraphReader):
+    """ """
 
-    def walk(self, graph: Graph):
+    @staticmethod
+    def max_col_of_code(code: str) -> int:
+        lines = code.split("\n")
+        max_col = 0
+        for i in lines:
+            if len(i) > max_col:
+                max_col = len(i)
+
+        return max_col
+
+    @staticmethod
+    def replace_slice_of_code(
+        code: str, new_code: str, start: int, end: int
+    ) -> str:
+        return code[:start] + new_code + code[end:]
+
+    @staticmethod
+    def add_node_to_code(
+        current_code: str, session_code: str, node: Node
+    ) -> str:
+        segment = get_segment_from_code(session_code, node)
+        segment_lines = segment.split("\n")
+        lines = current_code.split("\n")
+
+        # replace empty space
+        if node.lineno == node.end_lineno:
+            # if it's only a single line to be inserted
+            lines[
+                node.lineno - 1
+            ] = ProgramSlicer.replace_slice_of_code(
+                lines[node.lineno - 1],
+                segment,
+                node.col_offset,
+                node.end_col_offset,
+            )
+        else:
+            # if multiple lines need insertion
+            lines[
+                node.lineno - 1
+            ] = ProgramSlicer.replace_slice_of_code(
+                lines[node.lineno - 1],
+                segment_lines[0],
+                node.col_offset,
+                len(lines[node.lineno - 1]),
+            )
+
+            lines[
+                node.end_lineno - 1
+            ] = ProgramSlicer.replace_slice_of_code(
+                lines[node.end_lineno - 1],
+                segment_lines[-1],
+                0,
+                node.end_col_offset,
+            )
+
+            for i in range(1, len(segment_lines) - 1):
+                lines[node.lineno - 1 + i] = segment_lines[i]
+
+        return "\n".join(lines)
+
+    def validate(self, graph: Graph) -> None:
         pass
+
+    def walk(self, program: Graph) -> str:
+        session_code = program.code
+
+        nodes = [
+            node
+            for node in program.nodes
+            if node.lineno is not None and node.col_offset is not None
+        ]
+
+        num_lines = len(session_code.split("\n"))
+        code = "\n".join(
+            [" " * ProgramSlicer.max_col_of_code(session_code)]
+            * num_lines
+        )
+
+        for node in nodes:
+            code = ProgramSlicer.add_node_to_code(
+                code, session_code, node
+            )
+
+        code = re.sub(r"\n\s*\n", "\n\n", code)
+
+        # remove extra white spaces from end of each line
+        lines = code.split("\n")
+        for i in range(len(lines)):
+            lines[i] = lines[i].rstrip()
+        code = "\n".join(lines)
+
+        return code
