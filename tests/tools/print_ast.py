803610af895022cd2448ef020bfdc4644f14088c
Saul Shanabrook <s.shanabrook@gmail.com>
2021-09-20 16:24:51 -0700

    Add tracer printing to debug

--- tests/tools/print_ast.py
+++ tests/tools/print_ast.py
@@ -6,15 +6,20 @@ Pretty prints the AST of some Python code you pass in from the CLI
 from astpretty import pprint
 import ast
 import click
-
+from lineapy.transformer.node_transformer import NodeTransformer
+import astor
 
 
 
 @click.command()
 @click.argument("code")
 def linea_cli(code):
-    pprint(ast.parse(code))
-
-
+    ast_ = ast.parse(code)
+    print("*** AST ***")
+    pprint(ast_)
+    print("\n*** TRACER ***")
+    print(astor.to_source(NodeTransformer("dummy").visit(ast_)))
+    
+    
 if __name__ == "__main__":
     linea_cli()
