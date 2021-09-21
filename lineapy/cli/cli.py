5794bc2d08837c7ba53ec52fb43520bf8366e192
Yifan Wu <yifan1030@gmail.com>
2021-09-21 15:49:34 +0000

    fixed a merge bug

--- lineapy/cli/cli.py
+++ lineapy/cli/cli.py
@@ -30,13 +30,14 @@ def linea_cli(mode, session, file_name):
     execution_mode = ExecutionMode.__getitem__(str.upper(mode))
     if execution_mode == ExecutionMode.PROD:
         set_debug(False)
+    session_type = SessionType.__getitem__(str.upper(session))
     transformer = Transformer()
     try:
         lines = open(file_name, "r").readlines()
         original_code = "".join(lines)
         new_code = transformer.transform(
             original_code,
-            session_type=session,
+            session_type=session_type,
             session_name=file_name,
             execution_mode=execution_mode,
         )
