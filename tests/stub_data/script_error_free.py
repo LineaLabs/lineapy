517e7f82e434b1e3ecf0898aa7e75fbd4083a349
Yifan Wu <yifan1030@gmail.com>
2021-09-21 19:15:26 +0000

    removed testing file

--- tests/stub_data/script_error_free.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from PIL.Image import open
-
-df = pd.read_csv("simple_data.csv")
-plt.imsave("simple_data.png", df)
-img = open("simple_data.png")
-img = img.resize([200, 200])
