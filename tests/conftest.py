dae716847678e43710112f97ba31072dadfb7f0a
Saul Shanabrook <s.shanabrook@gmail.com>
2021-09-23 15:51:44 -0700

    Remove fancy exception handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -150,14 +150,7 @@ class ExecuteFixture:
         locals: dict[str, typing.Any] = {}
         bytecode = compile(trace_code, str(transformed_code_path), "exec")
 
-        # Handle exceptions on execing the transformed code,
-        # So that if an error is raised during evaluation of the nodes
-        # we can still get the graph to dump and compare before raising that
-        exec_transformed_exception: typing.Optional[Exception] = None
-        try:
-            exec(bytecode, {}, locals)
-        except Exception as e:
-            exec_transformed_exception = e
+        exec(bytecode, {}, locals)
 
         tracer: Tracer = locals["lineapy_tracer"]
 
@@ -180,9 +173,6 @@ class ExecuteFixture:
             )
             == self.snapshot(extension_class=PythonSnapshotExtension)
         )
-
-        if exec_transformed_exception is not None:
-            raise exec_transformed_exception
 
         return ExecuteResult(db, graph, tracer.executor)
 
