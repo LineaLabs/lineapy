086f8c424ee5b934c04ab900ac8b3a7811806b21
Saul Shanabrook <s.shanabrook@gmail.com>
2021-10-18 14:09:55 -0700

    Use temp directory workaround for examples

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+
+import pytest
 
 
 def test_cli_entrypoint():
@@ -34,27 +37,26 @@ def test_export_slice_housing():
 
 
 def test_kaggle_example1():
-    """
-    Verifies that the CLI command is aliased to the `lienapy` executable
-    """
+    os.chdir("examples")
+
     subprocess.check_call(
         [
             "lineapy",
-            "examples/kaggle_example1.py",
+            "kaggle_example1.py",
             "--slice",
             "mushroom feature importance",
         ]
     )
 
 
+@pytest.mark.xfail(reason="lambdas aren't supported")
 def test_kaggle_example2():
-    """
-    Verifies that the CLI command is aliased to the `lienapy` executable
-    """
+    os.chdir("examples")
+
     subprocess.check_call(
         [
             "lineapy",
-            "examples/kaggle_example2.py",
+            "kaggle_example2.py",
             "--slice",
             "nn for diabetes",
         ]
