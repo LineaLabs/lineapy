ee959e185329bd8efbde39fadb73ab6a400153d5
Saul Shanabrook <s.shanabrook@gmail.com>
2021-10-22 15:40:52 -0700

    Add way to save graph of notebook from CLI

--- tests/test_script.py
+++ tests/test_script.py
@@ -58,3 +58,16 @@ def test_kaggle_example2():
             "nn for diabetes",
         ]
     )
+
+
+def test_run_from_nbconvert():
+    # delete viz first, if it exists (-f exits cleanly either way)
+    subprocess.check_call(["rm", "-f", "tests/tmp.pdf"])
+    # Run the command that should populate the database
+    subprocess.check_call(
+        "env LINEA_VISUALIZATION_NAME=tmp jupyter nbconvert --to notebook --execute tests/test_untraced_notebook.ipynb --inplace --ExecutePreprocessor.extra_arguments=--IPKernelApp.extensions=lineapy --debug".split(
+            " "
+        )
+    )
+    # Delete it after (will exit cleanly only if it existed)
+    subprocess.check_call(["rm", "tests/tmp.pdf"])
