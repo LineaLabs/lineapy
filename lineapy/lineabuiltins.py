2ca05256b7e0608ae11cc35c0387aa3127d5675a
Saul Shanabrook <s.shanabrook@gmail.com>
2021-10-27 16:42:45 -0700

    Add docstring for record getitem dict

--- lineapy/lineabuiltins.py
+++ lineapy/lineabuiltins.py
@@ -88,6 +88,16 @@ _EXEC_EXPRESSION_SAVED_NAME = "__linea_expresion__"
 
 
 class RecordGetitemDict(dict):
+    """
+    A custom dict that records which keys have been succesfully accessed.
+
+    We cannot overload the `__setitem__` method, since Python will not respect
+    it for custom globals, but we can overload the __getitem__ method.
+
+    See https://stackoverflow.com/a/12185315/907060
+    which refers to https://bugs.python.org/issue14385
+    """
+
     def __init__(self, *args, **kwargs):
         self._getitems: list[str] = []
         super().__init__(*args, **kwargs)
