c46d0fcbed3bb1ac2ddd07849f02fca57aaecc44
Saul Shanabrook <s.shanabrook@gmail.com>
2021-10-28 15:30:14 -0700

    add frame code

--- lineapy/execution/executor.py
+++ lineapy/execution/executor.py
@@ -181,10 +181,19 @@ class Executor:
                 # the top level with a new one.
                 e.__traceback__ = TracebackType(
                     old_tb.tb_next,
-                    old_tb.tb_frame,
+                    make_frame(
+                        compile(
+                            node.source_location.source_code.code,
+                            str(node.source_location.source_code.location),
+                            "exec",
+                        )
+                    ),
                     old_tb.tb_lasti,
-                    old_tb.tb_lineno + 10,
+                    node.source_location.lineno,
                 )
+                # import pdb
+
+                # pdb.set_trace()
                 raise UserException() from e
             #     # traceback.
             #     # e.
@@ -319,3 +328,36 @@ class AccessedGlobals:
 
 
 SideEffects = Iterable[Union[MutatedNode, ViewOfNodes, AccessedGlobals]]
+
+
+import ctypes
+
+P_SIZE = ctypes.sizeof(ctypes.c_void_p)
+IS_X64 = P_SIZE == 8
+
+P_MEM_TYPE = ctypes.POINTER(ctypes.c_ulong if IS_X64 else ctypes.c_uint)
+
+ctypes.pythonapi.PyFrame_New.argtypes = (
+    P_MEM_TYPE,  # PyThreadState *tstate
+    P_MEM_TYPE,  # PyCodeObject *code
+    ctypes.py_object,  # PyObject *globals
+    ctypes.py_object,  # PyObject *locals
+)
+ctypes.pythonapi.PyFrame_New.restype = ctypes.py_object  # PyFrameObject*
+
+ctypes.pythonapi.PyThreadState_Get.argtypes = None
+ctypes.pythonapi.PyThreadState_Get.restype = P_MEM_TYPE
+
+
+# def greet():
+#     print("hello")
+
+
+def make_frame(code):
+
+    return ctypes.pythonapi.PyFrame_New(
+        ctypes.pythonapi.PyThreadState_Get(),  # thread state
+        ctypes.cast(id(code), P_MEM_TYPE),  # a code object
+        globals(),  # a dict of globals
+        locals(),  # a dict of locals
+    )
