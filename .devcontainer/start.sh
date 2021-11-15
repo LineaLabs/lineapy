b8e326f458975f7734a8fd68d3d4f7713090f847
shardul <shardul@linea.ai>
2021-11-15 21:33:12 +0000

    update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,12 +2,12 @@
 # Modified from https://docs.docker.com/config/containers/multi-service_container/
 
 # Start the first process
-make jupyterlab_start  &> /tmp/jupyterlab_log &
+make jupyterlab_start &
   
 # Start the second process
-make airflow_start &> /tmp/airflow_log & 
+make airflow_start &
   
-tail -f /tmp/jupyterlab_log /tmp/airflow_log &
+#tail -f /tmp/jupyterlab_log /tmp/airflow_log &
 
 # Wait for any process to exit
 wait -n
