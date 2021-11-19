6e98f2427ec6ff977634598a64663a669d8a35bd
Shardul Sardesai <shardul@linea.ai>
2021-11-19 14:35:18 -0800

    add failing tests.

---
+++ tests/end_to_end/test_starred.py
@@ -0,0 +1,26 @@
+import pytest
+
+CODE = """def func(a,b):
+    return a+b
+
+args = {'a':1, 'b':2}
+ret = func(**args)
+"""
+
+CODE2="""name="marcelo"
+it=iter(name)
+print(next(it))
+print(*it)
+"""
+
+@pytest.mark.xfail
+def test_starred_executes(execute):
+    ret = execute(CODE)
+    assert ret == 3
+
+
+@pytest.mark.xfail
+def test_starred2_executes(execute):
+    #should execute without failures
+    ret = execute(CODE2)
+    assert True
