d7a461e114e44bf354d5388c52f2b7bbf44f4ef2
Shardul Sardesai <shardul@linea.ai>
2021-11-22 11:27:26 -0800

    add visitor for boolop

--- tests/end_to_end/test_boolop.py
+++ tests/end_to_end/test_boolop.py
@@ -7,7 +7,7 @@ x = True and False
 
 S_BOOLOP2 = """x = 1>2
 y = 1<2
-#z = x and y
+z = x and y
 q = (1>2 or 1<2)
 """
 
@@ -21,4 +21,5 @@ def test_boolop_execute(execute):
 @pytest.mark.xfail
 def test_boolop2_execute(execute):
     res = execute(S_BOOLOP2)
-    assert res.values["q"] is False
+    assert res.values["q"] is True
+    assert res.values["z"] is False
