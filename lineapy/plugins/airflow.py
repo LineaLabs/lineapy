43dbdadc4cbc2f667ad6e69dc412efacf94213e0
marov <mike.arov@linea.ai>
2021-12-24 05:02:17 +0000

    Use template inheritence

--- lineapy/plugins/airflow.py
+++ lineapy/plugins/airflow.py
@@ -57,29 +57,21 @@ def slice_to_airflow(
     )
     templateEnv = Environment(loader=templateLoader)
 
-    AIRFLOW_IMPORTS_TEMPLATE = templateEnv.get_template("imports.jinja")
-
-    AIRFLOW_DAG_TEMPLATE = templateEnv.get_template("dag.jinja")
-
-    AIRFLOW_TASK_TEMPLATE = templateEnv.get_template("task.jinja")
-
-    AIRFLOW_FULL_TEMPLATE = templateEnv.get_template("full.jinja")
+    AIRFLOW_DAG_TEMPLATE = templateEnv.get_template("airflow_dag.jinja")
 
     # We split the code in import and code blocks and join them to full code test
     import_block, code_block, main_block = split_code_blocks(
         sliced_code, func_name
     )
 
-    full_code = AIRFLOW_FULL_TEMPLATE.render(
+    full_code = AIRFLOW_DAG_TEMPLATE.render(
         import_block=import_block,
-        airflow_import_block=AIRFLOW_IMPORTS_TEMPLATE.render(),
         working_dir_str=working_dir_str,
         code_block=code_block,
         variable=variable,
-        airflow_dag_block=AIRFLOW_DAG_TEMPLATE.render(DAG_NAME=func_name),
-        airflow_task_block=AIRFLOW_TASK_TEMPLATE.render(TASK_NAME=func_name),
+        DAG_NAME=func_name,
+        TASK_NAME=func_name,
     )
-    # Black lint
     black_mode = FileMode()
     black_mode.line_length = 79
     full_code = format_str(full_code, mode=black_mode)
