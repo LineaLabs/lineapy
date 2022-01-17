667b53fd25bdabcd06e86b761e2fd8c208ec90bd
Saul Shanabrook <s.shanabrook@gmail.com>
2022-01-17 17:28:53 -0500

    Update lineapy/execution/inspect_function.py

--- lineapy/execution/inspect_function.py
+++ lineapy/execution/inspect_function.py
@@ -244,7 +244,7 @@ def process_side_effect(
         new_side_effect.views = list(
             filter(lambda x: is_reference_mutable(x), new_side_effect.views)
         )
-        # If we don't habe at least two items to view each other, skip this one
+        # If we don't have at least two items to view each other, skip this one
         if len(new_side_effect.views) < 2:
             return None
         return new_side_effect
