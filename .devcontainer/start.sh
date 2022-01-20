2a7c38663560741975c7cd3ddc664504597e0f85
marov <mike.arov@gmail.com>
2022-01-20 09:26:49 -0800

    Codespaces rebuild fix (#488)
    * Codespaces rebuild fix
    
    * Airflow presistes DAG's current dir, which caused "File Not Found" error on os.chdir({{ working_dir_str }})

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # Modified from https://docs.docker.com/config/containers/multi-service_container/
 
+# turn on bash's job control
+set -m
+
 # Start the first process
 make jupyterlab_start &> /tmp/jupyterlab_log &
-  
+
 # Start the second process
 make airflow_home airflow_start &> /tmp/airflow_log &
-  
-tail -f /tmp/jupyterlab_log /tmp/airflow_log &
-
-# Wait for any process to exit
-wait -n
-  
-# Exit with status of process that exited first
-exit $?
