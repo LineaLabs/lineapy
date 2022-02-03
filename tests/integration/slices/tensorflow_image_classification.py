369a80f718e1ad7899b4c16061891458150882e7
Saul Shanabrook <s.shanabrook@gmail.com>
2022-02-03 14:08:50 -0500

    Cleanup TF slice

--- tests/integration/slices/tensorflow_image_classification.py
+++ tests/integration/slices/tensorflow_image_classification.py
@@ -6,27 +6,19 @@
 # To verify that linea produces the same slice, run:
 #  pytest -m integration --runxfail -vv 'tests/integration/test_slice.py::test_slice[tensorflow_image_classification]'
 
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
+import pathlib
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-import pathlib
 
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True)
+data_dir = tf.keras.utils.get_file(
+    "flower_photos", origin=dataset_url, untar=True
+)
 data_dir = pathlib.Path(data_dir)
-image_count = len(list(data_dir.glob("*/*.jpg")))
-print(image_count)
-roses = list(data_dir.glob("roses/*"))
-PIL.Image.open(str(roses[0]))
-PIL.Image.open(str(roses[1]))
-tulips = list(data_dir.glob("tulips/*"))
-PIL.Image.open(str(tulips[0]))
-PIL.Image.open(str(tulips[1]))
+
 batch_size = 32
 img_height = 180
 img_width = 180
@@ -47,28 +39,13 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=batch_size,
 )
 class_names = train_ds.class_names
-print(class_names)
-import matplotlib.pyplot as plt
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(class_names[labels[i]])
-        plt.axis("off")
-for image_batch, labels_batch in train_ds:
-    print(image_batch.shape)
-    print(labels_batch.shape)
-    break
 AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 normalization_layer = layers.Rescaling(1.0 / 255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-print(np.min(first_image), np.max(first_image))
 num_classes = len(class_names)
 model = Sequential(
     [
@@ -89,40 +66,18 @@ model.compile(
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
-model.summary()
 epochs = 10
 history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
-acc = history.history["accuracy"]
-val_acc = history.history["val_accuracy"]
-loss = history.history["loss"]
-val_loss = history.history["val_loss"]
-epochs_range = range(epochs)
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label="Training Accuracy")
-plt.plot(epochs_range, val_acc, label="Validation Accuracy")
-plt.legend(loc="lower right")
-plt.title("Training and Validation Accuracy")
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label="Training Loss")
-plt.plot(epochs_range, val_loss, label="Validation Loss")
-plt.legend(loc="upper right")
-plt.title("Training and Validation Loss")
-plt.show()
 data_augmentation = keras.Sequential(
     [
-        layers.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)),
+        layers.RandomFlip(
+            "horizontal", input_shape=(img_height, img_width, 3)
+        ),
         layers.RandomRotation(0.1),
         layers.RandomZoom(0.1),
     ]
 )
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-    for i in range(9):
-        augmented_images = data_augmentation(images)
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(augmented_images[0].numpy().astype("uint8"))
-        plt.axis("off")
+
 model = Sequential(
     [
         data_augmentation,
@@ -147,32 +102,3 @@ model.compile(
 model.summary()
 epochs = 15
 history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
-acc = history.history["accuracy"]
-val_acc = history.history["val_accuracy"]
-loss = history.history["loss"]
-val_loss = history.history["val_loss"]
-epochs_range = range(epochs)
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label="Training Accuracy")
-plt.plot(epochs_range, val_acc, label="Validation Accuracy")
-plt.legend(loc="lower right")
-plt.title("Training and Validation Accuracy")
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label="Training Loss")
-plt.plot(epochs_range, val_loss, label="Validation Loss")
-plt.legend(loc="upper right")
-plt.title("Training and Validation Loss")
-plt.show()
-sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file("Red_sunflower", origin=sunflower_url)
-img = tf.keras.utils.load_img(sunflower_path, target_size=(img_height, img_width))
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
-        class_names[np.argmax(score)], 100 * np.max(score)
-    )
-)
