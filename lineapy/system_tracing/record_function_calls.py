f2236091bbd502c5c36e8e73aa67ca40bb1f9919
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-11 13:34:07 -0700

    Add simple vis for demo

--- lineapy/system_tracing/record_function_calls.py
+++ lineapy/system_tracing/record_function_calls.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
 from contextlib import contextmanager
+from dataclasses import InitVar, dataclass, field
+from dis import Instruction, get_instructions
+from sys import settrace
 from types import CodeType
-from typing import Iterator, List
+from typing import Dict, Iterable, Iterator, List
 
+from rich.console import Console, Group
+from rich.pretty import Pretty
+from rich.table import Table
+
+from lineapy.system_tracing._op_stack import OpStack
 from lineapy.system_tracing.function_call import FunctionCall
 
 
@@ -12,4 +20,73 @@ def record_function_calls(code: CodeType) -> Iterator[List[FunctionCall]]:
     # TODO: use tracing
     function_calls: List[FunctionCall] = []
 
-    yield function_calls
+    try:
+        trace_fn = TraceFunc(code)
+        settrace(trace_fn)
+        yield function_calls
+    finally:
+        settrace(None)
+        trace_fn.visualize()
+
+
+@dataclass
+class TraceFunc:
+
+    code: InitVar[CodeType]
+    # Mapping of each code object that was passed in to its instructions, by offset.
+    code_to_offset_to_instruction: Dict[
+        CodeType, Dict[int, Instruction]
+    ] = field(init=False)
+
+    table: Table = field(
+        default_factory=lambda: Table(title="Bytecode Evaluations")
+    )
+
+    def __post_init__(self, code):
+        self.code_to_offset_to_instruction = {
+            code: {i.offset: i for i in get_instructions(code)}
+            for code in all_code_objects(code)
+        }
+
+        self.table.add_column("Name", justify="right", no_wrap=True)
+        self.table.add_column(
+            "Arg",
+            justify="right",
+        )
+        self.table.add_column(
+            "Stack",
+        )
+
+    def visualize(self):
+        console = Console()
+        console.print(self.table)
+
+    def __call__(self, frame, event, arg):
+        if frame.f_code not in self.code_to_offset_to_instruction:
+            return self
+        frame.f_trace_opcodes = True
+        if event != "opcode":
+            return self
+
+        offset_to_instructions = self.code_to_offset_to_instruction[
+            frame.f_code
+        ]
+
+        instruction = offset_to_instructions[frame.f_lasti]
+
+        op_stack = OpStack(frame)
+        has_stack = op_stack._len > 0
+
+        self.table.add_row(
+            instruction.opname,
+            instruction.argrepr,
+            Pretty(list(op_stack) if has_stack else []),
+        )
+        return self
+
+
+def all_code_objects(code: CodeType) -> Iterable[CodeType]:
+    yield code
+    for const in code.co_consts:
+        if isinstance(const, CodeType):
+            yield from all_code_objects(const)
