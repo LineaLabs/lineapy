4e31cdad01a388c52711124a363dcfd1f02912e7
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-16 15:03:41 -0700

    Black and mypy

--- lineapy/system_tracing/exec_and_record_function_calls.py
+++ lineapy/system_tracing/exec_and_record_function_calls.py
@@ -1,12 +1,12 @@
 from types import CodeType
-from typing import List, Mapping
+from typing import Dict, List
 
 from lineapy.system_tracing.function_call import FunctionCall
 from lineapy.system_tracing.record_function_calls import record_function_calls
 
 
 def exec_and_record_function_calls(
-    code: CodeType, globals_: Mapping[str, object]
+    code: CodeType, globals_: Dict[str, object]
 ) -> List[FunctionCall]:
     """
     Execute the code while recording all the function calls which originate from the code object.
