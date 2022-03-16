a6286ca1ac3cb4cd0b4712e1ec70dc39bba23ef6
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-16 11:34:05 -0700

    Respond to comments in system_tracing docstring

--- lineapy/system_tracing/__init__.py
+++ lineapy/system_tracing/__init__.py
@@ -5,12 +5,12 @@ and broken apart if need be.
 
 At a high level, users should:
 
-1. Use the `record_function_calls.py` context manager to record a number of function calls that happen during it,
-   limited to some code. This uses `sys.settrace` to trace every bytecode execution and `_op_stack.py` to look at
+1. Use the `record_function_calls.py` context manager to record a number of function calls that during the `with` statement,
+   limited recording to frames which are from the bytecode passed in. This uses `sys.settrace` to trace every bytecode execution and `_op_stack.py` to look at
    the bytecode stack during tracing. It translates different bytecode instructions into the corresponding Python
    function calls.
 2. Use `function_calls_to_side_effects.py` to translate the sequence of calls that were recorded into the side effects
-   produced on the nodes, by providing it with the initial values that were nodes.
+   produced on the nodes, by passing in the initial values that were nodes.
    In turn, this goes through a number of translation steps, first using `_function_calls_to_object_side_effects.py` to 
    to turn the function calls into the side effects in terms of the Python objects. For example, saying saying things like
    "The object [1, 2, 3] was mutated." Then, these are analyzed and turned into a list of side effects on nodes, like 
