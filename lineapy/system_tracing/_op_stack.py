99a1a68fe2de76ec879d103c7b57d7cb676d8132
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-23 16:29:07 -0700

    Remove some fields from opstack

--- lineapy/system_tracing/_op_stack.py
+++ lineapy/system_tracing/_op_stack.py
@@ -3,7 +3,7 @@ Modified from https://gist.github.com/crusaderky/cf0575cfeeee8faa1bb1b3480bc4a87
 """
 import sys
 from ctypes import POINTER, Structure, c_ssize_t, c_void_p, py_object, sizeof
-from typing import Any, Iterator, Optional, Sequence, Tuple, Union
+from typing import Any, Tuple
 
 __all__ = ("OpStack",)
 
@@ -37,58 +37,15 @@ F_VALUESTACK_OFFSET = sizeof(Frame) - 2 * PTR_SIZE
 F_STACKTOP_OFFSET = sizeof(Frame) - PTR_SIZE
 
 
-class OpStack(Sequence[Any]):
-    __slots__ = ("_frame", "_len")
+class OpStack:
+    __slots__ = ("_frame",)
 
     def __init__(self, frame):
         self._frame = Frame.from_address(id(frame))
-        stack_start_addr = c_ssize_t.from_address(
-            id(frame) + F_VALUESTACK_OFFSET
-        ).value
-        stack_top_addr = c_ssize_t.from_address(
-            id(frame) + F_STACKTOP_OFFSET
-        ).value
-        self._len = (stack_top_addr - stack_start_addr) // PTR_SIZE
 
     def __repr__(self) -> str:
-        if not self:
-            return "<OpStack> (empty)>"
-        return "<OpStack ({})>".format(len(self))
+        return "<OpStack>"
 
-    def __len__(self):
-        return self._len
-
-    def _preproc_slice(self, idx: Optional[int], default: int) -> int:
-        if idx is None:
-            return default
-        if idx < -self._len or idx >= self._len:
-            raise IndexError(idx)
-        if idx < 0:
-            return idx + self._len
-        return idx
-
-    def __getitem__(self, item: Union[int, slice]) -> Any:
-        if isinstance(item, int):
-            if item < -self._len or item >= self._len:
-                raise IndexError(item)
-            if item < 0:
-                return self._frame.f_stacktop[item]
-            return self._frame.f_valuestack[item]
-
-        if isinstance(item, slice):
-            item = slice(
-                self._preproc_slice(item.start, 0),
-                self._preproc_slice(item.stop, self._len),
-                item.step,
-            )
-            return self._frame.f_valuestack[item]
-
-        raise TypeError(item)
-
-    def __iter__(self) -> Iterator[Any]:
-        for i in range(self._len):
-            yield self._frame.f_valuestack[i]
-
-    def __reversed__(self) -> Iterator[Any]:
-        for i in range(self._len - 1, -1, -1):
-            yield self._frame.f_valuestack[i]
+    def __getitem__(self, item: int) -> Any:
+        if item < 0:
+            return self._frame.f_stacktop[item]
