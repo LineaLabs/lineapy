1a01576ad3e4613bc58e54bc555b416d0ae72b0c
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-29 10:03:19 -0700

    Fix mooreslaw manual slice

--- tests/integration/slices/numpy_mooreslaw.py
+++ tests/integration/slices/numpy_mooreslaw.py
@@ -12,8 +12,6 @@ import statsmodels.api as sm
 A_M = np.log(2) / 2
 B_M = np.log(2250) - A_M * 1971
 Moores_law = lambda year: np.exp(B_M) * np.exp(A_M * year)
-ML_1971 = Moores_law(1971)
-ML_1973 = Moores_law(1973)
 data = np.loadtxt("transistor_data.csv", delimiter=",", usecols=[1, 2], skiprows=1)
 year = data[:, 1]
 transistor_count = data[:, 0]
@@ -26,8 +24,6 @@ A = AB[0]
 B = AB[1]
 transistor_count_predicted = np.exp(B) * np.exp(A * year)
 transistor_Moores_law = Moores_law(year)
-y = np.linspace(2016.5, 2017.5)
-Moore_Model2017 = Moores_law(y)
 notes = "the arrays in this file are the result of a linear regression model\n"
 notes += """the arrays include
 year: year of manufacture
