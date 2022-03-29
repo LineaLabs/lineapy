01ee462651d81ef3ba828972017b32e3bbc24b69
Shardul Sardesai <shardul@linea.ai>
2022-03-29 11:41:53 -0700

    organize dags into folders. copy files in current folder when running to_airflow. currently only copy .csv, .cfg, .yaml files. can add more. this is only for demo so not worried about catching all filetypes.

--- tests/entrypoints/outputs/expected/multiple/sliced_housing_multiple.py
+++ tests/entrypoints/outputs/expected/multiple/sliced_housing_multiple.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def p_value():
-    assets = pd.read_csv("./ames_train_cleaned.csv")
+    assets = pd.read_csv("ames_train_cleaned.csv")
 
     def is_new(col):
         return col > 1970
@@ -14,3 +14,13 @@ def p_value():
     x = assets[["SalePrice", "Lot_Area", "Garage_Area"]]
     clf.fit(x, y)
     p = clf.predict([[100 * 1000, 10, 4]])
+
+
+def y():
+    assets = pd.read_csv("ames_train_cleaned.csv")
+
+    def is_new(col):
+        return col > 1970
+
+    assets["is_new"] = is_new(assets["Year_Built"])
+    y = assets["is_new"]
