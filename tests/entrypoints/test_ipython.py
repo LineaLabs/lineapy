59ff93bdd9b16c53c16cb090495ad22bb2030f03
Shardul Sardesai <shardul@linea.ai>
2022-03-30 13:33:38 -0700

    missed a few places

--- tests/entrypoints/test_ipython.py
+++ tests/entrypoints/test_ipython.py
@@ -44,16 +44,16 @@ def test_slice_artifact_inline(run_cell):
 def test_to_airflow_pymodule(python_snapshot, run_cell):
     assert run_cell("import lineapy") is None
     assert run_cell("a = [1, 2, 3]\nres = lineapy.save(a, 'a')") is None
-    py_module_path = run_cell("res.to_airflow()")
-    assert python_snapshot == py_module_path.read_text()
+    dag_root_path = run_cell("res.to_airflow()")
+    assert python_snapshot == (dag_root_path / "a.py").read_text()
 
 
 @pytest.mark.slow
 def test_to_airflow_dagmodule(python_snapshot, run_cell):
     assert run_cell("import lineapy") is None
     assert run_cell("a = [1, 2, 3]\nres = lineapy.save(a, 'a')") is None
-    py_module_path = run_cell("res.to_airflow()")
-    dag_module_path = py_module_path.parent / "a_dag.py"
+    dag_root_path = run_cell("res.to_airflow()")
+    dag_module_path = dag_root_path / "a_dag.py"
     assert python_snapshot == dag_module_path.read_text()
 
 
@@ -61,20 +61,20 @@ def test_to_airflow_dagmodule(python_snapshot, run_cell):
 def test_to_airflow_with_config_pymodule(python_snapshot, run_cell):
     assert run_cell("import lineapy") is None
     assert run_cell("a = [1, 2, 3]\nres = lineapy.save(a, 'a')") is None
-    py_module_path = run_cell(
+    dag_root_path = run_cell(
         "res.to_airflow(airflow_dag_config={'retries': 1, 'schedule_interval': '*/30 * * * *'})"
     )
-    assert python_snapshot == py_module_path.read_text()
+    assert python_snapshot == (dag_root_path / "a.py").read_text()
 
 
 @pytest.mark.slow
 def test_to_airflow_with_config_dagmodule(python_snapshot, run_cell):
     assert run_cell("import lineapy") is None
     assert run_cell("a = [1, 2, 3]\nres = lineapy.save(a, 'a')") is None
-    py_module_path = run_cell(
+    dag_root_path = run_cell(
         "res.to_airflow(airflow_dag_config={'retries': 1, 'schedule_interval': '*/30 * * * *'})"
     )
-    dag_module_path = py_module_path.parent / "a_dag.py"
+    dag_module_path = dag_root_path / "a_dag.py"
     assert python_snapshot == dag_module_path.read_text()
 
 
