239390eff0f8aa4489bee2a738f8993364a24945
Saul Shanabrook <s.shanabrook@gmail.com>
2022-03-31 17:02:42 -0700

    keep global cell cleanup just remove manual stopping

--- lineapy/editors/ipython_cell_storage.py
+++ lineapy/editors/ipython_cell_storage.py
@@ -15,6 +15,16 @@ from lineapy.data.types import JupyterCell, SourceCodeLocation
 _temp_dir: Optional[TemporaryDirectory] = None
 
 
+def cleanup_cells():
+    """
+    Remove the temporary directory with all of the files
+    """
+    global _temp_dir
+    if _temp_dir:
+        _temp_dir.cleanup()
+        _temp_dir = None
+
+
 def get_cell_path(cell: JupyterCell) -> Path:
     """
     Return the path to the temporary file for the given cell
