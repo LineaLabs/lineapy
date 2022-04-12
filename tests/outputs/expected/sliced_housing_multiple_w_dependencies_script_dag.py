a7aa64f2ac006201732904495a4ca3a9dbb646e3
Ming-Jer Lee <mingjerli@gmail.com>
2022-04-12 04:28:33 -0400

    Add test for script_dag

---
+++ tests/outputs/expected/sliced_housing_multiple_w_dependencies_script_dag.py
@@ -0,0 +1,9 @@
+import os
+
+import sliced_housing_multiple_w_dependencies
+
+if __name__ == "__main__":
+
+    sliced_housing_multiple_w_dependencies.p_value()
+
+    sliced_housing_multiple_w_dependencies.y()
---
+++ tests/test_pipeline_script.py
@@ -0,0 +1,86 @@
+import pathlib
+
+import pytest
+
+
+@pytest.mark.slow
+def test_slice_pythonscript(script_plugin):
+    """
+    Test the slice produced by script plugin against a snapshot.
+    """
+    script_plugin.sliced_pipeline_dag(
+        ["p value"],
+        "sliced_housing_simple",
+        "",
+        output_dir="outputs/generated",
+    )
+    for file_endings in [
+        ".py",
+        "_script_dag.py",
+        "_Dockerfile",
+        "_requirements.txt",
+    ]:
+        path = pathlib.Path(
+            "outputs/generated/sliced_housing_simple" + file_endings
+        )
+        path_expected = pathlib.Path(
+            "outputs/expected/sliced_housing_simple" + file_endings
+        )
+        assert path.read_text() == path_expected.read_text()
+
+
+@pytest.mark.slow
+def test_multiple_slices_pythonscript(python_snapshot, script_plugin):
+    """
+    Test producing ariflow DAG slicng several artifacts.
+    """
+    script_plugin.sliced_pipeline_dag(
+        ["p value", "y"],
+        "sliced_housing_multiple",
+        "",
+        output_dir="outputs/generated",
+    )
+    for file_endings in [
+        ".py",
+        "_script_dag.py",
+        "_Dockerfile",
+        "_requirements.txt",
+    ]:
+        path = pathlib.Path(
+            "outputs/generated/sliced_housing_multiple" + file_endings
+        )
+        path_expected = pathlib.Path(
+            "outputs/expected/sliced_housing_multiple" + file_endings
+        )
+        assert path.read_text() == path_expected.read_text()
+
+
+@pytest.mark.slow
+def test_multiple_slices_airflow_with_task_dependencies(
+    python_snapshot, script_plugin
+):
+    """
+    Test producing and ariflow DAG slicng several artifacts with task dependencies.
+    """
+    script_plugin.sliced_pipeline_dag(
+        ["p value", "y"],
+        "sliced_housing_multiple_w_dependencies",
+        "'p value' >> 'y'",
+        output_dir="outputs/generated",
+    )
+
+    for file_endings in [
+        ".py",
+        "_script_dag.py",
+        "_Dockerfile",
+        "_requirements.txt",
+    ]:
+        path = pathlib.Path(
+            "outputs/generated/sliced_housing_multiple_w_dependencies"
+            + file_endings
+        )
+        path_expected = pathlib.Path(
+            "outputs/expected/sliced_housing_multiple_w_dependencies"
+            + file_endings
+        )
+        assert path.read_text() == path_expected.read_text()
