c8de4dacc359234669fe93eb112b39d82bc335d8
Yifan Wu <yifan1030@gmail.com>
2022-04-14 18:15:07 +0000

    test refactor

--- tests/test_pipeline_script.py
+++ tests/test_pipeline_script.py
@@ -8,14 +8,37 @@ def check_requirements_txt(t1: str, t2: str):
 
 
 @pytest.mark.slow
-def test_slice_pythonscript(script_plugin):
+@pytest.mark.parametrize(
+    "artifact_names, dag_name, deps",
+    [
+        pytest.param(
+            ["p value"],
+            "sliced_housing_simple",
+            [],
+            id="sliced_housing_simple",
+        ),
+        pytest.param(
+            ["p value", "y"],
+            "sliced_housing_multiple",
+            [],
+            id="sliced_housing_multiple",
+        ),
+        pytest.param(
+            ["p value", "y"],
+            "sliced_housing_multiple_w_dependencies",
+            {"y": {"p value"}},
+            id="sliced_housing_multiple_w_dependencies",
+        ),
+    ],
+)
+def test_slice_pythonscript(artifact_names, dag_name, deps, script_plugin):
     """
     Test the slice produced by script plugin against a snapshot.
     """
     script_plugin.sliced_pipeline_dag(
-        ["p value"],
-        "sliced_housing_simple",
-        [],
+        artifact_names,
+        dag_name,
+        deps,
         output_dir="outputs/generated",
     )
     for file_endings in [
@@ -29,73 +52,6 @@ def test_slice_pythonscript(script_plugin):
         )
         path_expected = pathlib.Path(
             "outputs/expected/sliced_housing_simple" + file_endings
-        )
-        if file_endings != "_requirements.txt":
-            assert path.read_text() == path_expected.read_text()
-        else:
-            assert check_requirements_txt(
-                path.read_text(), path_expected.read_text()
-            )
-
-
-@pytest.mark.slow
-def test_multiple_slices_pythonscript(python_snapshot, script_plugin):
-    """
-    Test producing ariflow DAG slicng several artifacts.
-    """
-    script_plugin.sliced_pipeline_dag(
-        ["p value", "y"],
-        "sliced_housing_multiple",
-        {},
-        output_dir="outputs/generated",
-    )
-    for file_endings in [
-        ".py",
-        "_script_dag.py",
-        "_Dockerfile",
-        "_requirements.txt",
-    ]:
-        path = pathlib.Path(
-            "outputs/generated/sliced_housing_multiple" + file_endings
-        )
-        path_expected = pathlib.Path(
-            "outputs/expected/sliced_housing_multiple" + file_endings
-        )
-        if file_endings != "_requirements.txt":
-            assert path.read_text() == path_expected.read_text()
-        else:
-            assert check_requirements_txt(
-                path.read_text(), path_expected.read_text()
-            )
-
-
-@pytest.mark.slow
-def test_multiple_slices_airflow_with_task_dependencies(
-    python_snapshot, script_plugin
-):
-    """
-    Test producing and ariflow DAG slicng several artifacts with task dependencies.
-    """
-    script_plugin.sliced_pipeline_dag(
-        ["p value", "y"],
-        "sliced_housing_multiple_w_dependencies",
-        {"y": {"p value"}},
-        output_dir="outputs/generated",
-    )
-
-    for file_endings in [
-        ".py",
-        "_script_dag.py",
-        "_Dockerfile",
-        "_requirements.txt",
-    ]:
-        path = pathlib.Path(
-            "outputs/generated/sliced_housing_multiple_w_dependencies"
-            + file_endings
-        )
-        path_expected = pathlib.Path(
-            "outputs/expected/sliced_housing_multiple_w_dependencies"
-            + file_endings
         )
         if file_endings != "_requirements.txt":
             assert path.read_text() == path_expected.read_text()
