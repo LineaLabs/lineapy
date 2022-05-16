1568ca19e9a5e5e44475949d0f0ff02eeeba4e9f
Aubhro Sengupta <hello@aubhro.com>
2022-05-16 14:06:03 -0700

    Validate that custom annotation has correct ending when deleting
    File must end with .annotations.yaml to be deleted

--- lineapy/utils/config.py
+++ lineapy/utils/config.py
@@ -7,6 +7,8 @@ LOG_FILE_NAME = "linea.log"
 
 CUSTOM_ANNOTATIONS_FOLDER_NAME = "custom-annotations"
 
+CUSTOM_ANNOTATIONS_EXTENSION_NAME = ".annotations.yaml"
+
 
 logger = logging.getLogger(__name__)
 
