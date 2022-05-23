dc8786583ec6c48cb949a46ea559fa17b7e2e9c4
Shardul Sardesai <shardul@linea.ai>
2022-05-23 15:25:55 -0700

    matplotlit.plot access inside blackbox works now. The slice however is still not accurate. Adding a full test case with xfail to track the failing case.

--- tests/end_to_end/test_blackbox_tracing.py
+++ tests/end_to_end/test_blackbox_tracing.py
@@ -1,3 +1,7 @@
+import pytest
+
+LINEA_CODE = """import lineapy
+"""
 CODE = """import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,10 +22,22 @@ for i in range(3):
         linewidth=8,
         dash_joinstyle=dash_styles[i],
     )
+
+plt.xlim(0, 12), plt.ylim(-1, 2)
+plt.xticks([]), plt.yticks([])
+plt.savefig("output/dash_joinstyle.png", dpi=dpi)
+
+"""
+
+ARTIFACT_CODE = """
+artifact = lineapy.save(lineapy.file_system, "test_mplt")
 """
 
 
+@pytest.mark.xfail("libraries used inside a blackbox are not captured")
 def test_mplt_inside_blackbox_does_not_fail(execute):
-    res = execute(CODE)
-    assert res.success
+    # simply a test to check if the code runs without exceptions.
+    # Later on this will be edited to ensure that the slice is accurate.
+    res = execute(LINEA_CODE + CODE + ARTIFACT_CODE, snapshot=False)
+    assert res.values["artifact"].get_code() == CODE
     # assert res.values["fig"] is not None
