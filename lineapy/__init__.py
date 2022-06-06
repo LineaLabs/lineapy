0c1b1b6320a06b084955e15872a54eddde332149
Shardul Sardesai <shardul@linea.ai>
2022-06-06 13:50:15 -0700

    bump lineapy to 0.1.3

--- lineapy/__init__.py
+++ lineapy/__init__.py
@@ -27,7 +27,7 @@ __all__ = [
     "__version__",
 ]
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Create an ipython extension that starts and stops tracing
 # https://ipython.readthedocs.io/en/stable/config/extensions/index.html#writing-extensions
