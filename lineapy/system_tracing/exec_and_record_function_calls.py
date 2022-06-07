83fa39e58f41634ce80506dd7357199c42961c61
Aayan Kumar <aayan@linea.ai>
2022-06-07 14:17:30 -0700

    Adding comment for tracer fix

--- lineapy/system_tracing/exec_and_record_function_calls.py
+++ lineapy/system_tracing/exec_and_record_function_calls.py
@@ -13,6 +13,13 @@ def exec_and_record_function_calls(
 ) -> TraceFunc:
     """
     Execute the code while recording all the function calls which originate from the code object.
+
+    While recording function calls, we use sys.settrace() with LineaPy's tracer to extract relevant
+    information during the runtime of the user's code's function, and reset the tracer after the user
+    function has completed execution to prevent unnecessary logging.
+    However, to ensure LineaPy works correctly while debugging using VSCode, we first capture any
+    existing tracers using sys.gettrace(), perform our analysis using the LineaPy tracer, and reset
+     the existing tracer using sys.settrace()
     """
     logger.debug("Executing code")
     original_trace = gettrace()
