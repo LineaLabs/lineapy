7665e5b7bb7c57edd6f5549a9be6c107d389400a
Sangyoon Park <datumvitae@gmail.com>
2022-06-08 13:37:09 -0500

    LIN-427 Include runtime info in usage reporting (#682)
    * Include runtime info in usage reporting
    
    * Undo changes in config
    
    * Use env vars to identify runtime env; introduce (optional) custom tag
    
    * Use better naming schemes
    
    * Add an explanation for the strategy taken
    
    * Add type annotations
    
    * Use clearer runtime type name
    
    * Add a more detailed explanation/rationale for the strategy taken
    
    * Unroll some changes to pass mypy

--- lineapy/utils/analytics/usage_tracking.py
+++ lineapy/utils/analytics/usage_tracking.py
@@ -8,12 +8,14 @@ except ModuleNotFoundError:
 
 import json
 import logging
+import os
 import sys
 import uuid
 from dataclasses import asdict
 from functools import lru_cache
 
 import requests
+from IPython import get_ipython
 
 from lineapy.utils.analytics.event_schemas import AllEvents
 from lineapy.utils.config import options
@@ -25,7 +27,7 @@ LINEAPY_VERSION: str = importlib_metadata.version("lineapy")
 
 
 @lru_cache(maxsize=1)
-def _py_version():
+def _py_version() -> str:
     return "{major}.{minor}.{micro}".format(
         major=sys.version_info.major,
         minor=sys.version_info.minor,
@@ -33,16 +35,48 @@ def _py_version():
     )
 
 
-def _amplitude_url():
+@lru_cache(maxsize=1)
+def _runtime() -> str:
+    if get_ipython() is None:
+        runtime = "non-ipython"
+    else:
+        # Collect and concatenate all env var names to see
+        # if this concatenated string contains a pattern
+        # unique to each runtime.
+        # NOTE: We are not using ``get_ipython()`` as it does not really give the info we desire.
+        # For instance, it returns ``ipykernel.zmqshell.ZMQInteractiveShell`` object in both Databricks
+        # and local Jupyter; and it does not seem to contain attribute(s) to distinguish the two.
+        envars: str = ";".join(list(os.environ))
+        if "DATABRICKS_" in envars:
+            runtime = "ipython-databricks"
+        elif "COLAB_" in envars:
+            runtime = "ipython-colab"
+        elif "DEEPNOTE_" in envars:
+            runtime = "ipython-deepnote"
+        elif "BINDER_" in envars:
+            runtime = "ipython-binder"
+        else:
+            runtime = "ipython-unknown"
+
+    # Support optional custom tag which can be used to flag
+    # and discard certain events such as those from dev work
+    tag = os.environ.get("LINEAPY_RUNTIME_TAG")
+    if tag is not None:
+        return f"[{tag}]{runtime}"
+
+    return runtime
+
+
+def _amplitude_url() -> str:
     return "https://api.amplitude.com/2/httpapi"
 
 
-def _api_key():
+def _api_key() -> str:
     return "90e7eb47aee98355e46e6f6ed81d1a80"
 
 
 @lru_cache(maxsize=1)
-def _session_id():
+def _session_id() -> str:
     return str(uuid.uuid4())  # uuid that marks current python session
 
 
@@ -51,7 +85,7 @@ def do_not_track() -> bool:
     return str(options.get("do_not_track")).lower() == "true"
 
 
-def _send_amplitude_event(event_type, event_properties):
+def _send_amplitude_event(event_type: str, event_properties: dict):
     events = [
         {
             "event_type": event_type,
@@ -86,5 +120,6 @@ def track(event: AllEvents):
     event_properties = asdict(event)
     event_properties["py_version"] = _py_version()
     event_properties["lineapy_version"] = LINEAPY_VERSION
+    event_properties["runtime"] = _runtime()
 
     return _send_amplitude_event(event.__class__.__name__, event_properties)
