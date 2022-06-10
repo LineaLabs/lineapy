92ee3e9dee8a8c16d971788fa6297835e7c48b72
shardul <shardul@linea.ai>
2022-06-10 11:25:08 -0700

    Reload annotations (#654)
    * hot-reload annotations
    
    * Add tutorials to docs
    
    * clean up the demo notebooks. will add the text blurb if all this passes.
    
    * Add tutorials to docs
    
    * fix a few changes missed during renaming.
    
    * Add tutorials to docs
    
    Co-authored-by: Humble bot servant <infra@linea.ai>

--- lineapy/__init__.py
+++ lineapy/__init__.py
@@ -1,6 +1,13 @@
 import atexit
 
-from lineapy.api.api import artifact_store, delete, get, save, to_pipeline
+from lineapy.api.api import (
+    artifact_store,
+    delete,
+    get,
+    reload,
+    save,
+    to_pipeline,
+)
 from lineapy.data.graph import Graph
 from lineapy.data.types import SessionType, ValueType
 from lineapy.editors.ipython import start, stop, visualize
@@ -16,6 +23,7 @@ __all__ = [
     "get",
     "artifact_store",
     "delete",
+    "reload",
     "to_pipeline",
     "SessionType",
     "ValueType",
