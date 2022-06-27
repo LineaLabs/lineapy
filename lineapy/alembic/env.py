fe84167d145c9027f10c7fc15ece99ffba0e9c68
Josh Pollock <jopo@mit.edu>
2022-06-27 18:14:23 -0400

    add initial migration from empty table

--- lineapy/alembic/env.py
+++ lineapy/alembic/env.py
@@ -31,7 +31,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from lineapy.db.relational import Base
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
