aeccc62385fcb40d97f00cc15b9bdc2c19b32f1c
Sangyoon Park <datumvitae@gmail.com>
2022-06-27 08:59:03 -0500

    Refactor: Round 2

--- lineapy/graph_reader/graph_refactorer.py
+++ lineapy/graph_reader/graph_refactorer.py
@@ -1,7 +1,8 @@
 import itertools
 import string
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 from dataclasses import dataclass
+from typing import Set
 
 import networkx as nx
 
@@ -12,6 +13,16 @@ from lineapy.graph_reader.program_slice import (
     get_slice_graph,
     get_source_code_from_graph,
 )
+
+
+@dataclass
+class GraphSegment:
+    graph_segment: Graph
+    dependent_variables: Set[str]
+    created_variables: Set[str]
+    related_variables: Set[str]
+    required_variables: Set[str]
+    return_variables: Set[str]
 
 
 @dataclass
@@ -43,7 +54,7 @@ class GraphRefactorer:
 
         # Identify variable dependencies of each node in topological order
         node_dependency = dict()
-        for node_id in nx.topological_sort(full_graph.nx_graph):
+        for node_id in nx.topological_sort(full_nxgraph):
             predecessors = full_nxgraph.predecessors(node_id)
             node_dependency[node_id] = set()
             for p_node_id in predecessors:
@@ -51,67 +62,68 @@ class GraphRefactorer:
                 node_dependency[node_id] = node_dependency[node_id].union(dep)
 
         # Identify artifact nodes and topologically sort them
-        artifact_ordering = [
-            (artifact_dict[node_id], node_id)
+        artifact_ids_sorted = [
+            node_id
             for node_id in nx.topological_sort(full_nxgraph)
-            if artifact_dict.get(node_id) is not None
+            if node_id in artifact_dict
         ]
 
-        used_nodes = set()  # Track nodes that get ever used
-        nonoverlapping_artifact_graph = {}
-        artifact_variables = {}
-        for art_name, art_id in artifact_ordering:
-            artifact_subgraph = get_slice_graph(full_graph, [art_id])
-            artifact_nodes = set(artifact_subgraph.nx_graph.nodes) - used_nodes
+        # Construct a collection of graph segments
+        used_node_ids = set()  # Track nodes that get ever used
+        art_segment_dict = OrderedDict()
+        for art_id in artifact_ids_sorted:
+            # Identify "non-overlapping" nodes that solely belong to the artifact
+            art_name = artifact_dict[art_id]
+            art_slice = get_slice_graph(full_graph, [art_id])
+            art_node_ids = set(art_slice.nx_graph.nodes) - used_node_ids
+            art_graph_segment = full_graph.get_subgraph(
+                [full_graph.get_node(node_id) for node_id in art_node_ids]
+            )
 
-            nonoverlapping_artifact_graph[art_name] = full_graph.get_subgraph(
-                [
-                    full_graph.get_node(node_id)
-                    for node_id in set(artifact_nodes) - used_nodes
-                ]
-            )
-            used_nodes = used_nodes.union(
-                set(nonoverlapping_artifact_graph[art_name].nx_graph.nodes)
-            )
+            # Update used nodes for next iteration
+            used_node_ids = used_node_ids.union(art_node_ids)
+
+            # Calculate the artifact's variable relations
             dependent_variables = set(
                 itertools.chain.from_iterable(
-                    [node_dependency[node_id] for node_id in artifact_nodes]
+                    [node_dependency[node_id] for node_id in art_node_ids]
                 )
             )
             created_variables = set(
                 itertools.chain.from_iterable(
                     [
                         variable_dict.get(node_id, set())
-                        for node_id in artifact_nodes
+                        for node_id in art_node_ids
                     ]
                 )
             )
             related_variables = dependent_variables.union(created_variables)
             required_variables = related_variables - created_variables
-            return_variables = set([art_name])
+            return_variables = set([art_name])  # FIXME: Should be var name
 
-            artifact_variables[art_name] = {
-                "dependent_variables": dependent_variables,
-                "created_variables": created_variables,
-                "related_variables": related_variables,
-                "required_variables": required_variables,
-                "return_variables": return_variables,
-            }
+            # Create graph segment object for the artifact
+            art_segment_dict[art_name] = GraphSegment(
+                graph_segment=art_graph_segment,
+                dependent_variables=dependent_variables,
+                created_variables=created_variables,
+                related_variables=related_variables,
+                required_variables=required_variables,
+                return_variables=return_variables,
+            )
 
-        for i, (art_name, _) in enumerate(artifact_ordering):
-            required_variable = artifact_variables[art_name][
-                "required_variables"
-            ]
-            for p, (prev_art_name, _) in enumerate(artifact_ordering):
+        # Update `return_variables`` in each artifact segment to account for downstream requirements
+        for i, art_segment in enumerate(art_segment_dict.values()):
+            for p, prev_art_segment in enumerate(art_segment_dict.values()):
                 if p < i:
-                    artifact_variables[prev_art_name][
-                        "return_variables"
-                    ] = artifact_variables[prev_art_name][
-                        "return_variables"
-                    ].union(
-                        artifact_variables[prev_art_name][
-                            "related_variables"
-                        ].intersection(required_variable)
+                    variables_required_downstream = (
+                        prev_art_segment.related_variables.intersection(
+                            art_segment.required_variables
+                        )
+                    )
+                    prev_art_segment.return_variables = (
+                        prev_art_segment.return_variables.union(
+                            variables_required_downstream
+                        )
                     )
 
-        return artifact_variables
+        return art_segment_dict
