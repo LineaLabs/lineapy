4a2b09470729aefb0acb8223c439fab8221aee13
Ming-Jer Lee <mingjerli@gmail.com>
2022-07-12 11:30:44 -0400

    Add test cases

--- tests/unit/graph_reader/test_graph_refactorer.py
+++ tests/unit/graph_reader/test_graph_refactorer.py
@@ -3,16 +3,17 @@ from lineapy.utils.utils import prettify
 
 
 def test_refactor(execute):
-    #    Tests the following non-trivial example:
-    #    a0 --> (a0+=1) \                       /--> (f=c+7)
-    #                    \                     /
-    #                     >--> b=a*2+a0 --> (c=b+3) -------\
-    #                    /                                  \
-    #    (a=1) --> a+=1 /-----> d=a*4 --> e=d+5 --> (e+=6) --\
-    #                      \                                 \
-    #                        \--> a+=1 ------------------------\--> (g = c+e*2)--\
-    #                                                      \                     \
-    #                                                        \---------------------\--> (h=a+g)
+    # a0 --> (a0+=1) \                       /--> (f=c+7)
+    #                 \                     /
+    #                  >--> b=a*2+a0 --> (c=b+3) -------\
+    #                 /                                  \
+    # (a=1) --> a+=1 /-----> d=a*4 --> e=d+5 --> (e+=6) --\
+    #                    \                                 \
+    #                     \--> a+=1 ------------------------\--> (g = c+e*2)--\
+    #                                                    \                     \
+    #                                                     \---------------------\--> (h=a+g) --\
+    #                                                                                           >---> (z.append(h))
+    #                                                                                z = [1] --/
 
     """
     Node with parentheses are artifacts,
@@ -168,3 +169,141 @@ if __name__=="__main__":
         indentation=2, keep_lineapy_save=True
     )
     assert prettify(refactor_code_a0_c_h) == prettify(expection_result_a0_c_h)
+
+
+def test_module_import(execute):
+    code = """import lineapy
+art = {}
+import pandas
+df = pandas.DataFrame({'a':[1,2]})
+art['df'] = lineapy.save(df,'df')
+
+df2 = pandas.concat([df,df])
+art['df2'] = lineapy.save(df2,'df2')
+    """
+    expection_result_all = """import pandas
+
+def get_df():
+    df = pandas.DataFrame({"a": [1, 2]})
+    return df
+
+def get_df2(df):
+    df2 = pandas.concat([df, df])
+    return df2
+
+def pipeline():
+    df = get_df()
+    df2 = get_df2(df)
+    return df, df2
+
+if __name__=="__main__":
+    pipeline()
+"""
+
+    res = execute(code, snapshot=False)
+    art = res.values["art"]
+    assert len(res.values["art"]) == 2
+
+    sas = SessionArtifacts(list(art.values()))
+    refactor_code = sas.get_session_module_definition()
+    assert prettify(refactor_code) == prettify(expection_result_all)
+
+
+def test_module_import_alias(execute):
+    code = """import lineapy
+art = {}
+import pandas as pd
+df = pd.DataFrame({'a':[1,2]})
+art['df'] = lineapy.save(df,'df')
+
+df2 = pd.concat([df,df])
+art['df2'] = lineapy.save(df2,'df2')
+    """
+    expection_result_all = """import pandas as pd
+
+def get_df():
+    df = pd.DataFrame({"a": [1, 2]})
+    return df
+
+def get_df2(df):
+    df2 = pd.concat([df, df])
+    return df2
+
+def pipeline():
+    df = get_df()
+    df2 = get_df2(df)
+    return df, df2
+
+if __name__=="__main__":
+    pipeline()
+"""
+
+    res = execute(code, snapshot=False)
+    art = res.values["art"]
+    assert len(res.values["art"]) == 2
+
+    sas = SessionArtifacts(list(art.values()))
+    refactor_code = sas.get_session_module_definition()
+    assert prettify(refactor_code) == prettify(expection_result_all)
+
+
+def test_module_import_from(execute):
+    code = """import lineapy
+art = {}
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+# Load train data
+train_df = pd.read_csv("https://raw.githubusercontent.com/LineaLabs/lineapy/main/examples/tutorials/data/iris.csv")
+# Initiate the model
+mod = LinearRegression()
+# Fit the model
+mod.fit(
+    X=train_df[["petal.width"]],
+    y=train_df["petal.length"],
+)
+# Save the fitted model as an artifact
+art['model'] = lineapy.save(mod, "iris_model")
+# Load data to predict (assume it comes from a different source)
+pred_df = pd.read_csv("https://raw.githubusercontent.com/LineaLabs/lineapy/main/examples/tutorials/data/iris.csv")
+# Make predictions
+petal_length_pred =  mod.predict(X=pred_df[["petal.width"]])
+# Save the predictions
+art['pred'] = lineapy.save(petal_length_pred, "iris_petal_length_pred")
+    """
+    expection_result_all = """import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def get_iris_model():
+    train_df = pd.read_csv(
+        "https://raw.githubusercontent.com/LineaLabs/lineapy/main/examples/tutorials/data/iris.csv"
+    )
+    mod = LinearRegression()
+    mod.fit(
+        X=train_df[["petal.width"]],
+        y=train_df["petal.length"],
+    )
+    return mod
+
+def get_iris_petal_length_pred(mod):
+    pred_df = pd.read_csv(
+        "https://raw.githubusercontent.com/LineaLabs/lineapy/main/examples/tutorials/data/iris.csv"
+    )
+    petal_length_pred = mod.predict(X=pred_df[["petal.width"]])
+    return petal_length_pred
+
+def pipeline():
+    mod = get_iris_model()
+    petal_length_pred = get_iris_petal_length_pred(mod)
+    return mod, petal_length_pred
+
+if __name__=="__main__":
+    pipeline()
+"""
+
+    res = execute(code, snapshot=False)
+    art = res.values["art"]
+    assert len(res.values["art"]) == 2
+
+    sas = SessionArtifacts(list(art.values()))
+    refactor_code = sas.get_session_module_definition()
+    assert prettify(refactor_code) == prettify(expection_result_all)
