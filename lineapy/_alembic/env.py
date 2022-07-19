e1be0f9808d07c960d6f2208fa109772eba884bc
Andrew Cui <andrew.cui@linea.ai>
2022-07-19 08:28:50 -0700

    LIN-495 Fix logging and verbosity options
    Summary:
    This commit fixes the --verbose and --logging-level
    options for lineapy.
    
    Changes:
    1. Fix verbose option by setting correct key in lineapy_config options.set
    2. Fix logging level option by implementing logic to set logging level from provided parameter
    3. Fix logging by disabling alembic logging override
    4. Fix logging by moving off logging.basicConfig, which does not override previously set handler correctly
    5. Remove support for logging-level to be set by numbers
    6. Changed SQL logging level to match alembic.ini
    
    Test Plan:
    Run locally with
    lineapy --verbose
    lineapy --log-level CRITICAL
    lineapy --log-level DEBUG

--- lineapy/_alembic/env.py
+++ lineapy/_alembic/env.py
@@ -1,5 +1,3 @@
-from logging.config import fileConfig
-
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
@@ -11,11 +9,6 @@ from lineapy.utils.config import options
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
