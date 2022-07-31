5dfb0ba1580282d673c9973637ca87b6f0211c01
Ming-Jer Lee <mingjerli@gmail.com>
2022-07-30 23:03:44 -0400

    Add multi sessions test for ArtifactCollection

--- tests/unit/graph_reader/test_artifact_collection.py
+++ tests/unit/graph_reader/test_artifact_collection.py
@@ -64,7 +64,7 @@ from lineapy.utils.utils import prettify
         ),
     ],
 )
-def test_refactor(execute, input_script, artifact_list, expected_output):
+def test_one_session(execute, input_script, artifact_list, expected_output):
     """
     Test code refactor
     """
@@ -85,6 +85,79 @@ def test_refactor(execute, input_script, artifact_list, expected_output):
     # ac = ArtifactCollection(artifact_list)
     ac = res.values["ac"]
     refactor_result = ac.generate_module()
+    expected_result = pathlib.Path(
+        "tests/unit/graph_reader/expected/" + expected_output
+    ).read_text()
+    assert prettify(refactor_result) == prettify(expected_result)
+
+
+@pytest.mark.parametrize(
+    "input_script1, input_script2, artifact_list, expected_output, dependencies",
+    [
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "two_session_a0_b0",
+            {},
+            id="two_session_a0_b0",
+        ),
+        pytest.param(
+            "simple",
+            "complex",
+            ["b0", "a0"],
+            "two_session_b0_a0",
+            {},
+            id="two_session_b0_a0",
+        ),
+        pytest.param(
+            "simple",
+            "complex",
+            ["b0", "a0"],
+            "two_session_b0_a0_dependencies",
+            {"b0": {"a0"}},
+            id="two_session_b0_a0_dependencies",
+        ),
+        pytest.param(
+            "module_import_alias",
+            "module_import_from",
+            ["df", "iris_model", "iris_petal_length_pred"],
+            "two_session_df_iris",
+            {},
+            id="two_session_df_iris",
+        ),
+    ],
+)
+def test_two_session(
+    execute,
+    input_script1,
+    input_script2,
+    artifact_list,
+    expected_output,
+    dependencies,
+):
+    """
+    Test two sessions
+    """
+
+    code1 = pathlib.Path(
+        "tests/unit/graph_reader/inputs/" + input_script1
+    ).read_text()
+    res = execute(code1, snapshot=False)
+
+    code2 = pathlib.Path(
+        "tests/unit/graph_reader/inputs/" + input_script2
+    ).read_text()
+    res = execute(code2, snapshot=False)
+
+    artifact_string = ", ".join([f'"{x}"' for x in artifact_list])
+    code = (
+        "from lineapy.graph_reader.artifact_collection import ArtifactCollection\n"
+        + f"ac = ArtifactCollection([{artifact_string}])"
+    )
+    res = execute(code, snapshot=False)
+    ac = res.values["ac"]
+    refactor_result = ac.generate_module(dependencies)
     expected_result = pathlib.Path(
         "tests/unit/graph_reader/expected/" + expected_output
     ).read_text()
