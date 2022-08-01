0bc2d6ac7671b9c8ce002fe11e57b541027cb562
dorx <doris.s.xin@gmail.com>
2022-08-01 14:12:37 -0700

    update comments in graph.py

--- lineapy/data/graph.py
+++ lineapy/data/graph.py
@@ -13,18 +13,18 @@ from lineapy.utils.utils import listify, prettify
 class Graph(object):
     def __init__(self, nodes: List[Node], session_context: SessionContext):
         """
-        Graph represents a
-        It is constructed based on the following variables:
-        :param nodes: a list of nodes and the session context in wh
+        Graph is the core abstraction in LineaPy that is automatically generated
+        by capturing and analyzing user code. Nodes in Graph correspond to
+        variables and function calls from user code, and edges indicate
+        dependencies. This is the common IR upon which all LineaPy applications,
+        such as code cleanup and DAG generation, are built.
+
+        :param nodes: a list of LineaPy Nodes that make up the graph.
         :param session_context: the session context associated with the graph
 
-        NOTE:
-        # TODO: Possibly remove session context since we aren't using it anywhere
-        # we refer to the graph
-        - It makes sense to include session_context in the constructor of
-          the graph because the information in session_context is semantically
-          important to the notion of a Graph. Concretely, we are starting
-          to also use the code entry from the session_context.
+        NOTE: The information in session_context is semantically important to
+        the notion of a Graph. Concretely, we are starting to also use the code
+        entry from the session_context.
         """
         self.nodes: List[Node] = nodes
         self.ids: Dict[LineaID, Node] = dict((n.id, n) for n in nodes)
@@ -54,35 +54,39 @@ class Graph(object):
     @listify
     def visit_order(self) -> Iterator[Node]:
         """
-        Just using the line number as tie breaker for now since we don't have
-          a good way to track dependencies
-          Note that we cannot just use the line number to sort because
-            there are nodes created by us that do not have line numbers...
+        Just using the line number as the tie-breaker for now since we don't
+        have a good way to track dependencies.
+        Note that we cannot just use the line number to sort because there are
+        nodes created by us that do not have line numbers.
         """
-        # TODO: See if we could replace this with python's built in topological sort
-        # https://docs.python.org/3/library/graphlib.html
+        # TODO: Move this out of `Graph` and into classes that operate on top
+        #  of the graph.
 
-        # Before the introduction of Control Flow Analysis, the Linea Graph could be represented
-        # as a Directed Acyclic Graph with each node could be thought of as a computation with it's parents
-        # as it's dependencies. This was possible as without control flow analysis, we were only dealing with
-        # straight line code, which essentially is a sequence of instructions executed one after another with
-        # no jumps.
-        # However with the introduction of control flow, we need to introduce cycles in a graph to correspond
-        # to the cyclic dependencies possible, especially in loops, as the only way to avoid cycles would be
-        # to effectively unroll loops, which can become prohibitively expensive as the number of iterations in
-        # a loop increases.
+        # Before the introduction of Control Flow Analysis, the Linea Graph
+        # could be represented as a Directed Acyclic Graph where each node could
+        # be thought of as a computation with its parents as its dependencies.
+        # This was possible as without control flow analysis, we were only
+        # dealing with straight line code, which essentially is a sequence of
+        # instructions executed one after another with no jumps.
+        # However, with the introduction of control flow, we need to introduce
+        # cycles in a graph to correspond to the cyclic dependencies possible,
+        # especially in loops, as the only way to avoid cycles would be to
+        # effectively unroll loops, which can become prohibitively expensive as
+        # the number of iterations in a loop increases.
 
-        # Cycles in the graph would be enough to represent data/control dependencies, however while executing
-        # the graph we cannot depend on future information to be present. We need a way to break cycles while
-        # executing the graph, for which we currently resort to removing certain edges in the graph, to ensure
-        # we are able to obtain a topological ordering of the nodes, so that any node being executed depends
-        # on a value which is already defined.
+        # Cycles in the graph would be enough to represent data/control
+        # dependencies, however while executing the graph we cannot depend on
+        # future information to be present. We need a way to break cycles while
+        # executing the graph, for which we currently resort to removing certain
+        # edges in the graph, to ensure we are able to obtain a topological
+        # ordering of the nodes, so that any node being executed depends on a
+        # value which is already defined.
 
-        # For a Directed Acyclic Graph, generally, we want to traverse the graph in a way to maintain two
-        # constraints:
+        # For a Directed Acyclic Graph, generally, we want to traverse the graph
+        # in a way to maintain two constraints:
 
         # 1. All parents must be traversed before their children
-        # 2. If we have any freedom, those with earlier line number should come first
+        # 2. If permitted, nodes with smaller line numbers should come first
 
         # To do this, we do a breadth first traversal, keeping our queue ordered
         # by their line number. The sorting is done via the __lt__ method
@@ -90,22 +94,25 @@ class Graph(object):
         queue: PriorityQueue[Node] = PriorityQueue()
 
         # We also keep track of all nodes we have already added to the queue
-        # so that we don't add them again
+        # so that we don't add them again.
         seen: Set[LineaID] = set()
 
         # We also keep a mapping of each node to the number of parents left
         # which have not been visited yet.
-        # Note that we want to skip counting parents which are not part of our nodes
-        # This can happen we evaluate part of a graph, then another part.
+        # Note that we want to skip counting parents which are not part of our
+        # nodes. This can happen we evaluate part of a graph, then another part.
         # When evaluating the next part, we just have those nodes, so some
-        # of the parents will be missing, we assume they are already executed
+        # parents will be missing, we assume they are already executed.
 
-        # We also want to remove certain nodes which result in a cycle being made. In case a cycle is present,
-        # we would have a set of nodes, all of which have a nonzero number of non-executed parents. To find the
-        # next node to execute, we want one of the remaining nodes to have zero non-executed parents, which will
-        # indicate to us that the particular node can be executed as all required information is present.
+        # We also want to remove certain nodes which result in a cycle. In case
+        # a cycle is present, we would have a set of nodes, all of which have a
+        # nonzero number of non-executed parents. To find the next node to
+        # execute, we want one of the remaining nodes to have zero non-executed
+        # parents, which indicates to us that the particular node can be
+        # executed as all required information is present.
 
-        # We have certain cases of removing parents in order to ensure no cycles in the execution graph
+        # We have certain cases of removing parents in order to ensure no cycles
+        # in the execution graph.
         remaining_parents: Dict[str, int] = {}
 
         for node in self.nodes:
@@ -117,21 +124,23 @@ class Graph(object):
                 ]
             )
 
-            # Removing certain edges to ensure the graph for execution is acyclic, to generate a proper order
-            # for execution of nodes
+            # Removing certain edges to ensure the graph for execution is
+            # acyclic, to generate a proper order for execution of nodes
 
-            # Simply by reducing the counter n_remaining_counter by the appropriate amount is sufficient as we
-            # check whether n_remaining_parents for a particular node is zero for deciding whether it can be
-            # executed next, rather than modifying the edges in the actual graph
+            # Simply by reducing the counter `n_remaining_counter` by the
+            # appropriate amount is sufficient as we check whether n_remaining_
+            # parents for a particular node is zero for deciding whether it can
+            # be executed next, rather than modifying the edges in the graph.
 
-            # There is a cyclic dependency amongst and IfNode and ElseNode, both being connected to each other
-            # To break the cycle, we do not consider the connection from the IfNode to the ElseNode (ElseNode
+            # There is a cyclic dependency amongst and IfNode and ElseNode,
+            # both being connected to each other. To break the cycle, we do not
+            # consider the connection from the IfNode to the ElseNode (ElseNode
             # is not a dependency for IfNode to run)
             if isinstance(node, IfNode):
                 if node.companion_id is not None:
                     n_remaining_parents -= 1
 
-            # First we add all of the nodes to the queue which have no parents
+            # First we add all the nodes to the queue which have no parents.
             if n_remaining_parents == 0:
                 seen.add(node.id)
                 queue.put(node)
@@ -180,7 +189,11 @@ class Graph(object):
 
     def get_subgraph(self, nodes: List[Node]) -> "Graph":
         """
-        FIXME
+        Get a subgraph of the current graph induced by the input nodes.
+
+        :param nodes: The nodes in the subgraph
+        :return: A new `Graph` that contains `nodes` and the edges between
+        `nodes` in the current Graph and has the same session_context.
         """
         return Graph(nodes, self.session_context)
 
@@ -207,9 +220,9 @@ def queue_get_when(queue: "Queue[T]", filter_fn: Callable[[T], bool]) -> T:
     # We have to pop off a number of elements, stopping when we find one that
     # satisfies our conditional, since we can't iterate through a queue.
 
-    # Use a timeout of 0 for the gets, otherwise if we have some bug
-    # where we are trying to get off the queue and its empty it will just
-    # block forever. with a timeout of 0, it will raise an exception instead.
+    # Use a timeout of 0 for the gets. Otherwise, if we have some bug
+    # where we are trying to get off an empty queue, it will just
+    # block forever. With a timeout of 0, it will raise an exception instead.
     popped_off = [queue.get(timeout=0)]
     while not filter_fn(popped_off[-1]):
         popped_off.append(queue.get(timeout=0))
