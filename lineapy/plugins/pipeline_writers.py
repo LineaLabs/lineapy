e719965023214e827321f5f0c8c0729255efcb4b
Ming-Jer Lee <mingjerli@gmail.com>
2022-08-03 17:33:33 -0400

    WIP - Add AirflowPipelineWriter

--- lineapy/plugins/pipeline_writers.py
+++ lineapy/plugins/pipeline_writers.py
@@ -1,10 +1,14 @@
 import logging
+from enum import Enum
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
+
+from typing_extensions import TypedDict
 
 from lineapy.api.api_utils import de_lineate_code
 from lineapy.graph_reader.node_collection import NodeCollectionType
 from lineapy.graph_reader.session_artifacts import SessionArtifacts
+from lineapy.plugins.task import TaskGraph
 from lineapy.plugins.utils import load_plugin_template
 from lineapy.utils.logging_config import configure_logging
 from lineapy.utils.utils import prettify
@@ -114,5 +118,110 @@ if __name__ == "__main__":
         self._write_docker()
 
 
-class AirflowPipelineWriter(BasePipelineWriter):
-    pass
+AirflowDagConfig = TypedDict(
+    "AirflowDagConfig",
+    {
+        "owner": str,
+        "retries": int,
+        "start_date": str,
+        "schedule_interval": str,
+        "max_active_runs": int,
+        "catchup": str,
+    },
+    total=False,
+)
+
+
+class AirflowDagFlavor(Enum):
+    PythonOperatorPerSession = 1
+    # To be implemented for different flavor of airflow dags
+    # PythonOperatorPerArtifact = 2
+    # BashOperator = 3
+    # DockerOperator = 4
+    # KubernetesPodOperator = 5
+
+
+class AirflowPipelineWriter:
+    def __init__(
+        self,
+        session_artifacts_sorted: List[SessionArtifacts],
+        keep_lineapy_save: bool,
+        pipeline_name: str,
+        output_dir: str,
+    ) -> None:
+        self.session_artifacts_sorted = session_artifacts_sorted
+        self.keep_lineapy_save = keep_lineapy_save
+        self.pipeline_name = pipeline_name
+        self.output_dir = Path(output_dir, pipeline_name)
+
+        # Create output directory folder(s) if nonexistent
+        self.output_dir.mkdir(exist_ok=True, parents=True)
+
+        # We assume there is at least one SessionArtifacts object
+        self.db = self.session_artifacts_sorted[0].db
+
+    def _write_dag(
+        self,
+        airflow_dag_config: Optional[AirflowDagConfig] = {},
+        flavor: str = "PythonOperatorPerArtifact",
+    ) -> None:
+        airflow_dag_config = airflow_dag_config or {}
+
+        if (
+            AirflowDagFlavor[flavor]
+            == AirflowDagFlavor.PythonOperatorPerSession
+        ):
+            AIRFLOW_DAG_TEMPLATE = load_plugin_template(
+                "airflow_dag_PythonOperatorPerSession.jinja"
+            )
+            session_functions = [
+                f"run_session_including_{session_artifacts._get_first_artifact_name()}"
+                for session_artifacts in self.session_artifacts_sorted
+            ]
+            dependencies = {
+                session_functions[i + 1]: {session_functions[i]}
+                for i in range(len(session_functions) - 1)
+            }
+            task_graph = TaskGraph(
+                nodes=session_functions,
+                mapping={f: f for f in session_functions},
+                edges=dependencies,
+            )
+            full_code = AIRFLOW_DAG_TEMPLATE.render(
+                DAG_NAME=self.pipeline_name,
+                MODULE_NAME=self.pipeline_name + "_module",
+                OWNER=airflow_dag_config.get("owner", "airflow"),
+                RETRIES=airflow_dag_config.get("retries", 2),
+                START_DATE=airflow_dag_config.get("start_date", "days_ago(1)"),
+                SCHEDULE_INTERVAL=airflow_dag_config.get(
+                    "schedule_interval", "*/15 * * * *"
+                ),
+                MAX_ACTIVE_RUNS=airflow_dag_config.get("max_active_runs", 1),
+                CATCHUP=airflow_dag_config.get("catchup", "False"),
+                tasks=session_functions,
+                task_dependencies=task_graph.get_airflow_dependency(),
+            )
+            full_code = prettify(full_code)
+
+            # Write out file
+            file = self.output_dir / f"{self.pipeline_name}_dag.py"
+            file.write_text(prettify(full_code))
+
+            logger.info("Generated DAG file %s", file)
+
+    def _write_docker(self):
+        # Generate Dockerfile text
+        DOCKERFILE_TEMPLATE = load_plugin_template("dockerfile.jinja")
+        dockerfile_text = DOCKERFILE_TEMPLATE.render(
+            pipeline_name=self.pipeline_name
+        )
+
+        # Write out file
+        file = self.output_dir / f"{self.pipeline_name}_Dockerfile"
+        file.write_text(dockerfile_text)
+
+        logger.info("Generated Docker file %s", file)
+
+    def write_pipeline_files(self) -> None:
+        self._write_dag()
+        self._write_docker()
