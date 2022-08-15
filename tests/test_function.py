9f782d698274c8957f94298f5d5f23115b5761c8
Ming-Jer Lee <mingjerli@gmail.com>
2022-08-15 10:50:24 -0400

    Update test example comment

--- tests/test_function.py
+++ tests/test_function.py
@@ -27,16 +27,20 @@ lineapy.save(b,'prod_p')
     code = "import lineapy\nft = lineapy.get_function(['prod_p'], input_parameters=['a'])"
     res = execute(code, snapshot=False)
     ft = res.values["ft"]
-    assert ft()["prod_p"] == 2  # Default value for a
-    assert ft(a=5)["prod_p"] == 10  # New value for a
+    assert ft()["prod_p"] == 2  # Default value for a; i.e., 1x2
+    assert ft(a=5)["prod_p"] == 10  # New value for a; i.e., 5x2
 
     # Multiple input parameters
     code = "import lineapy\nft = lineapy.get_function(['prod_p'], input_parameters=['a', 'p'])"
     res = execute(code, snapshot=False)
     ft = res.values["ft"]
-    assert ft()["prod_p"] == 2  # Default value for a and p
-    assert ft(a=5)["prod_p"] == 10  # New value for a, default value for p
-    assert ft(a=5, p=3)["prod_p"] == 15  # New value for a, new value for p
+    assert ft()["prod_p"] == 2  # Default value for a and p; i.e., 1x2
+    assert (
+        ft(a=5)["prod_p"] == 10
+    )  # New value for a, default value for p; i.e., 5x2
+    assert (
+        ft(a=5, p=3)["prod_p"] == 15
+    )  # New value for a, new value for p;  i.e., 5x3
 
 
 def test_get_function_sanity(execute):
