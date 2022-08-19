d87910c3a23f3b083f8853e3ad04a457f1afb611
Yi Cheng <chengyidna@gmail.com>
2022-08-19 23:51:49 +0000

    up

--- ray_pipeline.py
+++ ray_pipeline.py
@@ -31,3 +31,77 @@ z_artifact = lineapy.get('my_z')
 sa = SessionArtifacts([x_artifact, y_artifact, z_artifact])
 writer = RayWorkflowPipelineWriter([sa], False, "pipeline", ".")
 writer._write_dag()
+
+# GENERATED CODE:
+"""
+
+import ray
+import ray.workflow as workflow
+import os
+assert os.environ.get("RAY_ADDRESS") is not None
+ray.init(address=os.environ["RAY_ADDRESS"])
+workflow.init()
+def get_my_x():
+    x = 1 + 2
+    return x
+
+def get_a_for_artifact_my_y_and_downstream():
+    a = 2 + 1
+    return a
+
+def get_my_y(a, x):
+    y = x * 10 + a
+    return y
+
+def get_my_z(a, x):
+    z = x + 20 + a
+    return z
+
+@ray.remote
+def _ray_my_x(*args):
+    inputs = {k: v for d in args for k, v in d.items()}
+    rets = get_my_x(**inputs)
+    outputs = ['x']
+    if not isinstance(rets, (list, tuple)): rets = [rets]
+    return dict(zip(outputs, rets))
+_ray_my_x_node = _ray_my_x.bind()
+
+@ray.remote
+def _ray_a_for_artifact_my_y_and_downstream(*args):
+    inputs = {k: v for d in args for k, v in d.items()}
+    rets = get_a_for_artifact_my_y_and_downstream(**inputs)
+    outputs = ['a']
+    if not isinstance(rets, (list, tuple)): rets = [rets]
+    return dict(zip(outputs, rets))
+_ray_a_for_artifact_my_y_and_downstream_node = _ray_a_for_artifact_my_y_and_downstream.bind()
+
+@ray.remote
+def _ray_my_y(*args):
+    inputs = {k: v for d in args for k, v in d.items()}
+    rets = get_my_y(**inputs)
+    outputs = ['y']
+    if not isinstance(rets, (list, tuple)): rets = [rets]
+    return dict(zip(outputs, rets))
+_ray_my_y_node = _ray_my_y.bind(_ray_a_for_artifact_my_y_and_downstream_node, _ray_my_x_node)
+
+@ray.remote
+def _ray_my_z(*args):
+    inputs = {k: v for d in args for k, v in d.items()}
+    rets = get_my_z(**inputs)
+    outputs = ['z']
+    if not isinstance(rets, (list, tuple)): rets = [rets]
+    return dict(zip(outputs, rets))
+_ray_my_z_node = _ray_my_z.bind(_ray_a_for_artifact_my_y_and_downstream_node, _ray_my_x_node)
+
+@ray.remote(**workflow.options(checkpoint=False))
+def gather(*args):
+    return {k: v for d in args for k, v in d.items()}
+
+_dag = gather.bind(_ray_my_x_node,_ray_my_y_node,_ray_my_z_node)
+workflow_id = os.environ.get("WORKFLOD_ID")
+# if not set, random id will be generated.
+# we might want:
+# 1. pass workflow id with cli.
+# 2. choose to resume or just start a new one.
+print(ray.workflow.run(_dag, workflow_id))
+"""
