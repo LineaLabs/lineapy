6f1a7d15b89a4a349ec5b7cbcb55b12f61a611f5
Andrew Cui <andycui97@gmail.com>
2022-10-28 13:54:03 -0700

    Change snapshot and e2e tests for pipelines

--- tests/test_pipeline_run_airflow.py
+++ tests/test_pipeline_run_airflow.py
@@ -1,17 +1,169 @@
 import subprocess
+from pathlib import Path
 
 import pytest
+
+from lineapy.api.models.linea_artifact import get_lineaartifactdef
+from lineapy.data.types import PipelineType
+from lineapy.graph_reader.artifact_collection import ArtifactCollection
+from lineapy.plugins.pipeline_writer_factory import PipelineWriterFactory
 
 
 @pytest.mark.airflow
 @pytest.mark.slow
-def test_run_airflow_dag(virtualenv, tmp_path):
+@pytest.mark.parametrize(
+    "input_script1, input_script2, artifact_list, pipeline_name, dependencies, dag_config, input_parameters",
+    [
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "script_pipeline_a0_b0",
+            {},
+            {},
+            [],
+            id="script_pipeline_a0_b0",
+        ),
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "airflow_pipeline_a0_b0",
+            {},
+            {},
+            [],
+            id="airflow_pipeline_a0_b0",
+        ),
+        pytest.param(
+            "simple",
+            "",
+            ["a", "b0"],
+            "airflow_pipeline_a_b0_inputpar",
+            {},
+            {"dag_flavor": "PythonOperatorPerArtifact"},
+            ["b0"],
+            id="airflow_pipeline_a_b0_input_parameter_per_artifact",
+        ),
+        pytest.param(
+            "simple",
+            "",
+            ["a", "b0"],
+            "airflow_pipeline_a_b0_inputpar_session",
+            {},
+            {"dag_flavor": "PythonOperatorPerSession"},
+            ["b0"],
+            id="airflow_pipeline_a_b0_input_parameter_per_session",
+        ),
+        pytest.param(
+            "simple_twovar",
+            "",
+            ["pn"],
+            "airflow_pipeline_two_input_parameter",
+            {},
+            {"dag_flavor": "PythonOperatorPerArtifact"},
+            ["n", "p"],
+            id="airflow_pipeline_two_input_parameter",
+        ),
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "airflow_pipeline_a0_b0_dependencies",
+            {"a0": {"b0"}},
+            {},
+            [],
+            id="airflow_pipeline_a0_b0_dependencies",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["p value"],
+            "airflow_pipeline_housing_simple",
+            {},
+            {},
+            [],
+            id="airflow_pipeline_housing_simple",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["y", "p value"],
+            "airflow_pipeline_housing_multiple",
+            {},
+            {},
+            [],
+            id="airflow_pipeline_housing_multiple",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["y", "p value"],
+            "airflow_pipeline_housing_w_dependencies",
+            {"p value": {"y"}},
+            {},
+            [],
+            id="airflow_pipeline_housing_w_dependencies",
+        ),
+        pytest.param(
+            "complex",
+            "",
+            ["f", "h"],
+            "airflow_complex_h_perart",
+            {},
+            {"dag_flavor": "PythonOperatorPerArtifact"},
+            [],
+            id="airflow_complex_h_perartifact",
+        ),
+    ],
+)
+def test_run_airflow_dag(
+    virtualenv,
+    tmp_path,
+    linea_db,
+    execute,
+    input_script1,
+    input_script2,
+    artifact_list,
+    pipeline_name,
+    dependencies,
+    dag_config,
+    input_parameters,
+):
     """
     Verifies that the "--airflow" CLI command produces a working Airflow DAG
     by running the DAG locally.
 
     Depends on snapshot being available from previous test.
     """
+
+    code1 = Path(
+        "tests", "unit", "graph_reader", "inputs", input_script1
+    ).read_text()
+    execute(code1, snapshot=False)
+
+    if input_script2 != "":
+        code2 = Path(
+            "tests", "unit", "graph_reader", "inputs", input_script2
+        ).read_text()
+        execute(code2, snapshot=False)
+
+    # Write out pipeline files
+    artifact_def_list = [get_lineaartifactdef(art) for art in artifact_list]
+    artifact_collection = ArtifactCollection(
+        linea_db, artifact_def_list, input_parameters=input_parameters
+    )
+
+    # Construct pipeline writer
+    pipeline_writer = PipelineWriterFactory.get(
+        pipeline_type=PipelineType["AIRFLOW"],
+        artifact_collection=artifact_collection,
+        dependencies=dependencies,
+        pipeline_name=pipeline_name,
+        output_dir=tmp_path,
+        dag_config=dag_config,
+    )
+    pipeline_writer.write_pipeline_files()
+
     airflow_home = tmp_path / "airflow"
     airflow_home.mkdir()
     dags_home = airflow_home / "dags"
@@ -21,15 +173,22 @@ def test_run_airflow_dag(virtualenv, tmp_path):
     # NOTE: We can't leave them in the tests folder, since there are other
     # files in there that airflow will attempt to import, and fail, since
     # those dependencies are not in the virtualenv.
-    subprocess.check_call(
-        [
-            "cp",
-            "-f",
-            "tests/unit/plugins/expected/airflow_pipeline_housing_simple/airflow_pipeline_housing_simple_dag.py",
-            "tests/unit/plugins/expected/airflow_pipeline_housing_simple/airflow_pipeline_housing_simple_module.py",
-            str(dags_home),
-        ]
-    )
+
+    ex = None
+    try:
+        ex = subprocess.check_call(
+            [
+                "cp",
+                "-f",
+                f"{tmp_path}/{pipeline_name}_module.py",
+                f"{tmp_path}/{pipeline_name}_dag.py",
+                str(dags_home),
+            ]
+        )
+    except Exception as e:
+        print(ex)
+        print(e)
+        raise ValueError
 
     # Run airflow in new virtual env so we don't end up with version conflicts
     # with lineapy deps
@@ -43,7 +202,7 @@ def test_run_airflow_dag(virtualenv, tmp_path):
     # We create a new DB for airflow for testing, so it's reproducible
     virtualenv.run("airflow db init", capture=False)
     virtualenv.run(
-        "airflow dags test airflow_pipeline_housing_simple_dag 2020-10-19",
+        f"airflow dags test {pipeline_name}_dag 2020-10-19",
         capture=False,
         # Run in current root lineapy so that relative paths are accurate
         # cd=".",
--- tests/test_pipeline_run_script.py
+++ tests/test_pipeline_run_script.py
@@ -1,32 +1,116 @@
 import os
 import subprocess
+from pathlib import Path
 
 import pytest
 
+from lineapy.api.models.linea_artifact import get_lineaartifactdef
+from lineapy.data.types import PipelineType
+from lineapy.graph_reader.artifact_collection import ArtifactCollection
+from lineapy.plugins.pipeline_writer_factory import PipelineWriterFactory
 
+
+@pytest.mark.parametrize(
+    "input_script1, input_script2, artifact_list, pipeline_name, dependencies, dag_config, input_parameters",
+    [
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "script_pipeline_a0_b0",
+            {},
+            {},
+            [],
+            id="script_pipeline_a0_b0",
+        ),
+        pytest.param(
+            "simple",
+            "complex",
+            ["a0", "b0"],
+            "script_pipeline_a0_b0_dependencies",
+            {"a0": {"b0"}},
+            {},
+            [],
+            id="script_pipeline_a0_b0_dependencies",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["p value"],
+            "script_pipeline_housing_simple",
+            {},
+            {},
+            [],
+            id="script_pipeline_housing_simple",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["y", "p value"],
+            "script_pipeline_housing_multiple",
+            {},
+            {},
+            [],
+            id="script_pipeline_housing_multiple",
+        ),
+        pytest.param(
+            "housing",
+            "",
+            ["y", "p value"],
+            "script_pipeline_housing_w_dependencies",
+            {"p value": {"y"}},
+            {},
+            [],
+            id="script_pipeline_housing_w_dependencies",
+        ),
+    ],
+)
 @pytest.mark.slow
-def test_run_script_dag(virtualenv, tmp_path):
+def test_run_script_dag(
+    virtualenv,
+    tmp_path,
+    linea_db,
+    execute,
+    input_script1,
+    input_script2,
+    artifact_list,
+    pipeline_name,
+    dependencies,
+    dag_config,
+    input_parameters,
+):
     """
     Verifies that the DAGs we produce do run successfully.
     """
 
-    # FIXME - we need a more basic test than this that does not depend on pandas and sklearn etc.
-    # installing them takes way too long right now.
+    code1 = Path(
+        "tests", "unit", "graph_reader", "inputs", input_script1
+    ).read_text()
+    execute(code1, snapshot=False)
 
-    # Copy the dag and the data
-    # NOTE: Don't want to leave them in the tests folder, since there are other
-    # files we need to copy without which the dag will fail.
-    subprocess.check_call(
-        [
-            "cp",
-            "-f",
-            "tests/unit/plugins/expected/script_pipeline_housing_simple/script_pipeline_housing_simple_module.py",
-            str(tmp_path),
-        ]
+    if input_script2 != "":
+        code2 = Path(
+            "tests", "unit", "graph_reader", "inputs", input_script2
+        ).read_text()
+        execute(code2, snapshot=False)
+
+    # Write out pipeline files
+    artifact_def_list = [get_lineaartifactdef(art) for art in artifact_list]
+    artifact_collection = ArtifactCollection(
+        linea_db, artifact_def_list, input_parameters=input_parameters
     )
+
+    # Construct pipeline writer
+    pipeline_writer = PipelineWriterFactory.get(
+        pipeline_type=PipelineType["SCRIPT"],
+        artifact_collection=artifact_collection,
+        dependencies=dependencies,
+        pipeline_name=pipeline_name,
+        output_dir=tmp_path,
+        dag_config=dag_config,
+    )
+    pipeline_writer.write_pipeline_files()
 
     os.chdir(str(tmp_path))
 
-    subprocess.check_call(
-        ["python", "script_pipeline_housing_simple_module.py"]
-    )
+    subprocess.check_call(["python", f"{pipeline_name}_module.py"])
