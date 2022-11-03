6f9e6c23c15d2b34b868513e0661e48260e0f818
Lazar Gugleta <37106022+lazargugleta@users.noreply.github.com>
2022-11-03 19:07:26 +0100

    Add opencv package annotation (#838)
    * opencv package annotation
    
    * skip tests if cv2 module not installed
    
    * remove prettify from test_opencv.py

---
+++ tests/end_to_end/test_opencv.py
@@ -0,0 +1,44 @@
+import pytest
+
+cv2 = pytest.importorskip("cv2")
+
+
+@pytest.mark.slow
+def test_opencv_knn(execute):
+    code = """import cv2 as cv
+import numpy as np
+numpy.random.seed(0)
+trainData = np.random.randint(0,100,(25,2)).astype(np.float32)
+responses = np.random.randint(0,2,(25,1)).astype(np.float32)
+red = trainData[responses.ravel()==0]
+blue = trainData[responses.ravel()==1]
+newcomer = np.random.randint(0,100,(1,2)).astype(np.float32)
+knn = cv.ml.KNearest_create()
+knn.train(trainData, cv.ml.ROW_SAMPLE, responses)
+ret, results, neighbours ,dist = knn.findNearest(newcomer, 3)
+"""
+    res = execute(code, artifacts=["results", "neighbours", "dist"])
+    assert res.values["results"].tolist() == [[0]]
+    assert res.values["neighbours"].tolist() == [[1.0, 0, 0]]
+    assert res.values["dist"].tolist() == [[85.0, 109.0, 353.0]]
+
+
+@pytest.mark.slow
+def test_opencv_lr(execute):
+    code = """import cv2 as cv
+import numpy as np
+numpy.random.seed(0)
+trainData = np.random.randint(0,100,(25,2)).astype(np.float32)
+responses = np.random.randint(0,2,(25,1)).astype(np.float32)
+red = trainData[responses.ravel()==0]
+blue = trainData[responses.ravel()==1]
+newcomer = np.random.randint(0,100,(1,2)).astype(np.float32)
+lr = cv.ml.LogisticRegression_create()
+lr.setTrainMethod(cv.ml.LogisticRegression_MINI_BATCH)
+lr.setMiniBatchSize(1)
+lr.setIterations(100)
+lr.train(trainData, cv.ml.ROW_SAMPLE, responses)
+ret, results = lr.predict(newcomer)
+"""
+    res = execute(code, artifacts=["results"])
+    assert res.values["results"].tolist() == [[1.0]]
