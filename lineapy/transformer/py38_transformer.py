b009bf25ca6fea79be81f589fa70f8d88b4ebdeb
Shardul Sardesai <shardul@linea.ai>
2022-11-02 22:44:37 -0700

    add type ignores for 3.8 related classes

--- lineapy/transformer/py38_transformer.py
+++ lineapy/transformer/py38_transformer.py
@@ -5,10 +5,12 @@ from lineapy.transformer.base_transformer import BaseTransformer
 
 class Py38Transformer(BaseTransformer):
     def visit_Index(self, node: ast.Index) -> ast.AST:
-        return self.visit(node.value)
+        # ignoring types because these classes were entirely removed without backward support in 3.9
+        return self.visit(node.value)  # type: ignore
 
     def visit_ExtSlice(self, node: ast.ExtSlice) -> ast.Tuple:
-        elem_nodes = [self.visit(elem) for elem in node.dims]
+        # ignoring types because these classes were entirely removed without backward support in 3.9
+        elem_nodes = [self.visit(elem) for elem in node.dims]  # type: ignore
         return ast.Tuple(
             elts=list(elem_nodes),
         )
