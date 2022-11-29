87471adce41d54bd90843b72da32821bfc4d0e7a
Shardul Sardesai <shardul@linea.ai>
2022-11-28 20:23:08 -0800

    refactor some functions

--- lineapy/graph_reader/utils.py
+++ lineapy/graph_reader/utils.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from typing import Dict, List, Set
+from itertools import groupby
+from typing import Dict, List
 
 from lineapy.api.models.linea_artifact import LineaArtifact, LineaArtifactDef
 from lineapy.data.graph import Graph
@@ -35,25 +36,39 @@ def get_artifacts_grouped_by_session(
 
     """
     # TODO: This seems to do three things:
-    # 1) Check if artifact name has been repeated in the input list
-    # 2) converts artifactdef to a linea artfact by fetching it from the db
+    # 2)
     # 3) groups these initialized artifacts into a dict with session ids as key
     #
     # I think the function is doing too much and needs to be fragmented into three with each handling these tasks
 
+    # TODO - migrate this check outside this function.
+    # Check if artifact name has been repeated in the input list
+    # original comment: Check no two target artifacts have the same name
+    check_duplicates(artifact_entries)
+
     artifacts_grouped_by_session: Dict[
         LineaID, List[LineaArtifact]
     ] = defaultdict(list)
-    seen_artifact_names: Set[str] = set()
-    for art_def in artifact_entries:
-        # Check no two target artifacts have the same name
-        if art_def["artifact_name"] in seen_artifact_names:
-            raise KeyError(
-                "Artifact %s is duplicated", art_def["artifact_name"]
-            )
-        # Retrieve artifact and put it in the right group
-        art = LineaArtifact.get_artifact_from_def(db, art_def)
-        artifacts_grouped_by_session[art._session_id].append(art)
-        seen_artifact_names.add(art_def["artifact_name"])
+
+    # converts artifactdef to a linea artfact by fetching it from the db
+    all_linea_artifacts = [
+        LineaArtifact.get_artifact_from_def(db, art_def)
+        for art_def in artifact_entries
+    ]
+    for session_id, artifacts_by_session in groupby(
+        all_linea_artifacts, lambda d: d._session_id
+    ):
+        # TODO: collapsing here for now but we could very well yield as a generator.
+        # leaving it for future refactors
+        artifacts_grouped_by_session[session_id] = list(artifacts_by_session)
 
     return artifacts_grouped_by_session
+
+
+def check_duplicates(artifact_entries: List[LineaArtifactDef]):
+    all_names = [art_def["artifact_name"] for art_def in artifact_entries]
+    if len(all_names) != len(set(all_names)):
+        raise KeyError(
+            # TODO - make this error more specific. Should tell you which calling list has the duplicates
+            "Duplicate artifacts added"
+        )
