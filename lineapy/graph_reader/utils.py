2634f98cda62353830e8f6ab1144689340a7bf5f
Shardul Sardesai <shardul@linea.ai>
2022-11-30 12:27:32 -0800

    docstring updated and fixed comments [skip-ci]

--- lineapy/graph_reader/utils.py
+++ lineapy/graph_reader/utils.py
@@ -25,7 +25,10 @@ def is_import_node(graph: Graph, node_id: LineaID) -> bool:
 def get_db_artifacts_from_artifactdef(
     db: RelationalLineaDB, artifact_entries: List[LineaArtifactDef]
 ) -> List[LineaArtifact]:
-    # converts artifactdef list to a linea artfact list by fetching it from the db
+    """
+    Converts LineaArtifactDef list to a LineaArtfact list by initializing the artifacts from the db provided
+    Artifact entries are specified as name and optionally version as the end user would specify.
+    """
     return [
         LineaArtifact.get_artifact_from_def(db, art_def)
         for art_def in artifact_entries
@@ -36,15 +39,10 @@ def get_artifacts_grouped_by_session(
     all_linea_artifacts: List[LineaArtifact],
 ) -> Iterable[Tuple[LineaID, List[LineaArtifact]]]:
     """
-    Get LineaArtifact from each artifact entry and group by the Session they belong to.
-
-    Artifact entries are specified as name and optionally version as the end user would specify.
-
     This helper function is used to group target and reuse_precomputed artifacts so that we can
     create SessionArtifacts for each Session.
-
     """
-    # This function groups artifacts into a dict with session ids as key
+    # This function returns a generator
     #
     for session_id, artifacts_by_session in groupby(
         all_linea_artifacts, lambda d: d._session_id
@@ -57,5 +55,5 @@ def check_duplicates(artifact_entries: List[LineaArtifactDef]):
     if len(all_names) != len(set(all_names)):
         raise KeyError(
             # TODO - make this error more specific. Should tell you which calling list has the duplicates
-            "Duplicate artifacts added"
+            "Duplicate artifacts found in input"
         )
