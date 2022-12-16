c2aa8c65902f391d354c6a1db16f0df8533f41c8
Sangyoon Park <datumvitae@gmail.com>
2022-12-16 15:12:04 +0900

    Set up MkDocs

--- setup.py
+++ setup.py
@@ -103,12 +103,11 @@ benchmark_libs = [
 ]
 
 doc_libs = [
-    "sphinx",
-    "nbsphinx",
-    "sphinx_rtd_theme",
-    "sphinx-autobuild",
-    "sphinx_design",
     "pandoc",
+    "mkdocs",
+    "mkdocs-material",
+    "mkdocstrings",
+    "mkdocstrings-python",
 ]
 
 typing_libs = [
